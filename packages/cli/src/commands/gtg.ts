1f7a427915bfa21f9376d96b93fa75f0e4ef8184
andygout <andygout@hotmail.co.uk>
2021-02-22 15:41:09 +0000

    Refactor gtg task script

--- packages/cli/src/commands/gtg.ts
+++ packages/cli/src/commands/gtg.ts
@@ -1,39 +1,45 @@
 import {Command, flags} from '@oclif/command'
 import fetch from 'node-fetch'
 
+const TWO_MINUTES = 2 * 60 * 1000;
+
 function waitForOk (url: string) {
-	return new Promise(function (resolve, reject) {
-		let timeout: number;
-		let checker: number;
-		function checkGtg () {
-			console.log(`⏳ polling: ${url}`);
-			fetch(url, { timeout: 2000, follow: 0 })
-				.then(function (response) {
-					if (response.ok) {
-						console.log(`✅ ${url} ok!`);
-						clearTimeout(timeout);
-						clearInterval(checker);
-						resolve();
-					} else {
-						console.log(`❌ ${url} not ok`);
-					}
-				})
-				.catch(error => {
-					if (error.type && error.type === 'request-timeout') {
-						console.log(`👋 Hey, ${url} doesn't seem to be responding yet, so there's that. You're amazing, by the way. I don't say that often enough. But you really are.`); // eslint-disable-line no-console
-					}
-					else {
-						reject(`😿 ${url} Error: ${error}`);
-						clearInterval(checker);
-					}
-				});
+
+	let timeout: NodeJS.Timeout;
+	let checker: NodeJS.Timeout;
+
+	async function checkGtg () {
+
+		console.log(`⏳ polling: ${url}`);
+
+		try {
+			const response = await fetch(url, { timeout: 2000, follow: 0 })
+
+			if (response.ok) {
+				console.log(`✅ ${url} ok!`);
+				clearTimeout(timeout);
+				clearInterval(checker);
+				return Promise.resolve();
+			} else {
+				console.log(`❌ ${url} not ok`);
+			}
+		} catch (error) {
+			if (error.type && error.type === 'request-timeout') {
+				console.log(`👋 Hey, ${url} doesn't seem to be responding yet, so there's that. You're amazing, by the way. I don't say that often enough. But you really are.`); // eslint-disable-line no-console
+			}
+			else {
+				return Promise.reject(`😿 ${url} Error: ${error}`);
+				clearInterval(checker);
+			}
 		}
-		checker = setInterval(checkGtg, 3000);
-		timeout = setTimeout(function () {
-			reject(`😢 ${url} did not respond with an ok response within two minutes.`);
-			clearInterval(checker);
-		}, 2*60*1000);
-	});
+	}
+
+	checker = setInterval(checkGtg, 3000);
+
+	timeout = setTimeout(function () {
+		Promise.reject(`😢 ${url} did not respond with an ok response within two minutes.`);
+		clearInterval(checker);
+	}, TWO_MINUTES);
 
 };
 
