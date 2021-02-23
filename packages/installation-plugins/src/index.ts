02b81ef332f5f5810a8b4003542a079dd55e8d15
Kara Brightwell <kara@153.io>
2021-02-23 09:48:00 +0000

    factor out plugin loading functions

--- packages/installation-plugins/src/index.ts
+++ packages/installation-plugins/src/index.ts
@@ -1,5 +1,26 @@
-import {Hook, Config, Plugin, IConfig} from '@oclif/config'
+import {Hook, Plugin, IConfig} from '@oclif/config'
 import * as readPkgUp from 'read-pkg-up'
+
+async function loadPlugin(config: IConfig, plugin: string, root: string) {
+   const instance = new Plugin({
+      name: plugin,
+      type: 'consumer',
+      root: process.cwd(),
+   })
+
+   await instance.load()
+   config.plugins.push(instance)
+}
+
+async function loadPlugins(config: IConfig, plugins: string[], root: string) {
+   await Promise.all(
+      plugins.map(
+         async plugin => {
+            loadPlugin(config, plugin, root)
+         }
+      )
+   )
+}
 
 const hook: Hook<'init'> = async function(options) {
    const result = await readPkgUp()
@@ -10,20 +31,7 @@ const hook: Hook<'init'> = async function(options) {
       dep => dep.startsWith('@dotcom-tool-kit')
    )
 
-   await Promise.all(
-      plugins.map(
-         async plugin => {
-            const instance = new Plugin({
-               name: plugin,
-               type: 'consumer',
-               root: process.cwd(),
-            })
-
-            await instance.load()
-            options.config.plugins.push(instance)
-         }
-      )
-   )
+   await loadPlugins(options.config, plugins, process.cwd())
 }
 
 export default hook
