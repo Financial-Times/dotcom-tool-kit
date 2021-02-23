716a71e3398cdb68ee26efed510660059891a3d1
andygout <andygout@hotmail.co.uk>
2021-02-23 13:24:38 +0000

    Add Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,30 +17,5 @@ module.exports = {
       }
     }
   },
-  rules: {
-    'eqeqeq': 'error',
-    'guard-for-in': 'error',
-    // 'import/no-unresolved': [2, { commonjs: true, caseSensitive: true }],
-    'import/no-named-as-default': 0,
-    'indent': ['error', 'tab', {'SwitchCase': 1}],
-    'new-cap': 'off',
-    'no-caller': 'error',
-    'no-console': 'error',
-    'no-extend-native': 'error',
-    'no-irregular-whitespace': 'error',
-    'no-loop-func': 'error',
-    'no-multi-spaces': 'error',
-    'no-return-await': 'warn',
-    'no-trailing-spaces': 'error',
-    'no-undef': 'error',
-    'no-underscore-dangle': 'off',
-    'no-unused-vars': 'error',
-    'no-var': 'error',
-    'one-var': ['error', 'never'],
-    'quotes': ['error', 'single'],
-    'semi': ['warn', 'always'],
-    'space-before-function-paren': ['error', 'always'],
-    'wrap-iife': 'error'
-  },
   overrides: []
 }
