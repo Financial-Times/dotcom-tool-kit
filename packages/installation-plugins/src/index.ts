423505b3a10e946f8f8699b5e902c51068d998f4
Kara Brightwell <kara@153.io>
2021-02-23 11:25:32 +0000

    use oclif's internal loadPlugins function which actually works

--- packages/installation-plugins/src/index.ts
+++ packages/installation-plugins/src/index.ts
@@ -1,45 +1,33 @@
 import {Hook, Plugin, IConfig} from '@oclif/config'
 import * as readPkgUp from 'read-pkg-up'
 
-async function loadPlugin(config: IConfig, plugin: string, root: string, parent?: Plugin) {
-   const instance = new Plugin({
-      name: plugin,
-      type: 'consumer',
-      root: process.cwd(),
-   })
-
-   await instance.load()
-   config.plugins.push(instance)
-
-   if (parent) {
-      instance.parent = parent
-      if (!parent.children) parent.children = []
-      parent.children.push(instance)
-    }
-
-    await loadPlugins(config, instance.pjson.oclif.plugins || [], instance.root, instance)
+// according to Oclif's type definitions, loadPlugins isn't there on
+// options.config. but we know it is. so tell Typescript it can use
+// it by using a type predicate function to make it into this interface
+interface PluginLoader {
+   loadPlugins(root: string, type: string, plugins: string[]): Promise<void>
 }
 
-async function loadPlugins(config: IConfig, plugins: string[], root: string, parent?: Plugin) {
-   await Promise.all(
-      plugins.map(
-         async plugin => {
-            loadPlugin(config, plugin, root)
-         }
-      )
-   )
+function canLoadPlugins(config: any): config is PluginLoader {
+   if(typeof config.loadPlugins === 'function') {
+      return true
+   }
+
+   return false
 }
 
-const hook: Hook<'init'> = async function(options) {
+const hook: Hook<'init'> = async function (options) {
    const result = await readPkgUp()
-   if(!result) return
+   if (!result) return
 
    const { devDependencies = {} } = result.packageJson
    const plugins = Object.keys(devDependencies).filter(
       dep => dep.startsWith('@dotcom-tool-kit')
    )
 
-   await loadPlugins(options.config, plugins, process.cwd())
+   if (canLoadPlugins(options.config)) {
+      await options.config.loadPlugins(process.cwd(), 'consumer', plugins)
+   }
 }
 
 export default hook
