2b7e27c6b820eaca563f9a58d89cb4ae248aa5cc
Kara Brightwell <kara@153.io>
2021-02-24 12:59:27 +0000

    add create-plugin script and scaffold a heroku plugin with it

---
+++ packages/heroku-postbuild/src/commands/heroku-postbuild.ts
@@ -0,0 +1,11 @@
+import { Command } from '@oclif/command'
+
+export default class Heroku-Postbuild extends Command {
+   static description = ''
+   static flags = {}
+   static args = []
+
+   async run() {
+
+   }
+}
---
+++ scripts/create-plugin.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const path = require('path')
+const { execSync } = require('child_process')
+
+const name = process.argv[2]
+const camelCaseName = name.replace(/(^|-)./g, char => char.toUpperCase())
+const dir = path.join('packages', name)
+
+console.log('📂 creating folder')
+fs.mkdirSync(dir)
+process.chdir(dir)
+
+console.log('📦 initialising package')
+execSync('npm init -y --scope @dotcom-tool-kit')
+
+console.log('📥 installing @oclif/command')
+execSync('npm install @oclif/command')
+
+console.log('🔣 adding oclif metadata to package.json')
+const pkg = JSON.parse(fs.readFileSync('package.json'))
+pkg.oclif = { commands: './lib/commands' }
+fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2))
+
+console.log('🏗 scaffolding command')
+fs.mkdirSync('src/commands', {recursive: true})
+
+fs.writeFileSync(`src/commands/${name}.ts`,
+`import { Command } from '@oclif/command'
+
+export default class ${camelCaseName} extends Command {
+   static description = ''
+   static flags = {}
+   static args = []
+
+   async run() {
+
+   }
+}`)
+
+console.log('🌊 byeee~')
