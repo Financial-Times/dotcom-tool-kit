dcdbd31ad437e033887a1b8c7eb5e15ac798d29c
circle-auto-deploy <estefania.morton@ft.com>
2021-02-25 13:53:38 +0000

    gtg 500 test

--- packages/cli/test/commands/gtg.test.ts
+++ packages/cli/test/commands/gtg.test.ts
@@ -1,19 +1,24 @@
-/**
- * @jest-environment node
- */
 /* eslint-disable no-console */
-/* eslint-disable quotes */
 import { expect, test } from '@oclif/test'
-
-const wait = (ms = 10) => new Promise(resolve => setTimeout(resolve, ms))
 
 describe('gtg', () => {
   test
   .nock('http://ft-next-health-eu.herokuapp.com', api => api.get('/__gtg').reply(200))
-  .stdout()
+  .stdout({print: true})
   .command(['gtg', '--app', 'ft-next-health-eu'])
   .timeout(5000)
   .it(`tests an app's __gtg endopoint`, async (ctx) => {
+    expect(ctx.stdout).to.contain('http://ft-next-health-eu.herokuapp.com/__gtg ok!')
+  })
+})
+
+describe('gtg 500', () => {
+  test
+  .nock('http://ft-next-health-eu.herokuapp.com', api => api.get('/__gtg').reply(500))
+  .stdout({print: true})
+  .command(['gtg', '--app', 'ft-next-health-eu'])
+  .timeout(5000)
+  .it(`tests an app's __gtg endopoint when it returns a 500`, async (ctx) => {
     expect(ctx.stdout).to.contain('http://ft-next-health-eu.herokuapp.com/__gtg')
   })
 })
