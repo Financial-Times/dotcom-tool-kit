a0a906ee53e383f29ab9a0d1efb5a3ea05cc562d
Kara Brightwell <kara@153.io>
2021-04-26 16:56:13 +0100

    get eslint file list from plugin options

--- packages/eslint/src/commands/eslint.ts
+++ packages/eslint/src/commands/eslint.ts
@@ -1,14 +1,22 @@
 import { Command } from '@oclif/command'
 import { ESLint } from 'eslint'
 
+interface EslintOptions {
+   files: string[] | string
+}
+
 export default class EslintCommand extends Command {
    static description = ''
    static flags = {}
    static args = []
 
+   options: EslintOptions = {
+      files: '**/*.js'
+   }
+
    async run() {
       const eslint = new ESLint()
-      const results = await eslint.lintFiles('**/*.js')
+      const results = await eslint.lintFiles(this.options.files)
       const formatter = await eslint.loadFormatter("stylish")
       const resultText = formatter.format(results);
 
