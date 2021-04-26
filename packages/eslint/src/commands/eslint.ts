ae6eb8a7b5e3d37cb1ed56d70d93012a3162b519
Kara Brightwell <kara@153.io>
2021-04-26 11:12:10 +0100

    implement eslint command
    Co-authored-by: leannecornish-ft <leannecornish-ft@users.noreply.github.com>
    Co-authored-by: D Budge  <debugwand@users.noreply.github.com>
    Co-authored-by: Estefanía morton <emortong@users.noreply.github.com>

--- packages/eslint/src/commands/eslint.ts
+++ packages/eslint/src/commands/eslint.ts
@@ -1,11 +1,17 @@
 import { Command } from '@oclif/command'
+import { ESLint } from 'eslint'
 
-export default class Eslint extends Command {
+export default class EslintCommand extends Command {
    static description = ''
    static flags = {}
    static args = []
 
    async run() {
+      const eslint = new ESLint()
+      const results = await eslint.lintFiles('**/*.js')
+      const formatter = await eslint.loadFormatter("stylish")
+      const resultText = formatter.format(results);
 
+      console.log(resultText)
    }
 }
