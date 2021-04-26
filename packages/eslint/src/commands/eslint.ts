7bb978cf0d9b3c360d7afd874ba0103f1d433f63
Kara Brightwell <kara@153.io>
2021-04-26 10:53:04 +0100

    add skeleton eslint plugin
    Co-authored-by: leannecornish-ft <leannecornish-ft@users.noreply.github.com>
    Co-authored-by: D Budge  <debugwand@users.noreply.github.com>
    Co-authored-by: Estefanía morton <emortong@users.noreply.github.com>

---
+++ packages/eslint/src/commands/eslint.ts
@@ -0,0 +1,11 @@
+import { Command } from '@oclif/command'
+
+export default class Eslint extends Command {
+   static description = ''
+   static flags = {}
+   static args = []
+
+   async run() {
+
+   }
+}
