58adac45bece1a4a55feba3815a573b164d82be1
Leanne <leanne.cornish@ft.com>
2021-04-27 13:02:46 +0100

    add test directory option for mocha

--- packages/mocha/src/commands/mocha.ts
+++ packages/mocha/src/commands/mocha.ts
@@ -2,18 +2,27 @@ import { Command } from '@oclif/command'
 import Mocha from 'mocha'
 import fs from 'fs'
 
+interface MochaOptions {
+   testDir: string
+}
+
 export default class MochaCommand extends Command {
    static description = ''
    static flags = {}
    static args = []
 
+   options: MochaOptions = {
+      testDir: './test'
+   }
+
    async run() {
       const mocha = new Mocha()
-      let fileNames = fs.readdirSync('./test')
+      let fileNames = fs.readdirSync(this.options.testDir)
       fileNames.forEach((file: string) =>{
-         mocha.addFile(`./test/${file}`)
+         mocha.addFile(`${this.options.testDir}/${file}`)
       })
       mocha
          .run()
+         //TODO: error handling
    }
 }
--- packages/sandbox/mocha_tests/test1.js
+++ packages/sandbox/mocha_tests/test1.js
@@ -4,7 +4,7 @@ const assert = require('assert')
 describe(' Variable', function() {
     it('should return true', function() {
         const testVar = 1
-        assert.deepStrictEqual(testVar, 1)
+        assert.deepStrictEqual(testVar, 2)
         }
     )
 })
