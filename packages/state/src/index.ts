ae47ce650b18f35ba5bb80e6fe3b38c79043ac41
Leanne <leanne.cornish@ft.com>
2021-05-17 09:29:24 +0100

    genericised the read and write state function paras and return

--- packages/state/src/index.ts
+++ packages/state/src/index.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs'
 
 const stateFile = '.toolkitstate.json'
 
-export function readState(stage: string, item: string): string | null {
+export function readState<T>(stage: T, item: T): T | null {
   if (fs.existsSync(stateFile)) {
     const readStateContent = JSON.parse(fs.readFileSync(stateFile, {encoding: 'utf-8'}))
     try {
@@ -14,19 +14,19 @@ export function readState(stage: string, item: string): string | null {
   return null
 }
 
-export function writeState(stage: string, item: string, value: string): string | null {
+export function writeState<T>(stage: T, item: T, value: T): T | null {
   if (fs.existsSync(stateFile)) {
     const readStateContent = JSON.parse(fs.readFileSync(stateFile, {encoding: 'utf-8'}))
     if (readStateContent[stage]) {
       readStateContent[stage][item] = value
     } else {
-      readStateContent[stage] = {[item] : value}
+      readStateContent[stage] = {item : value}
     }
     fs.writeFileSync(stateFile, JSON.stringify(readStateContent, null, 2) )
   } else {
     const data = {
-        [stage]: {
-          [item] : value
+        stage: {
+          item : value
         }
       }
     fs.writeFileSync(stateFile, JSON.stringify(data, null, 2))
