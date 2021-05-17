a857c1183ed00c4b33654d9e895eab8631627f01
Leanne <leanne.cornish@ft.com>
2021-05-17 15:12:17 +0100

    removing relative path to package

--- packages/cli/src/commands/gtg.ts
+++ packages/cli/src/commands/gtg.ts
@@ -1,34 +1,32 @@
-console.log('hello')
+import { Command, flags } from '@oclif/command'
+import waitForOk from '@dotcom-tool-kit/wait-for-ok'
 
-// import { Command, flags } from '@oclif/command'
-// import waitForOk from '../../../wait-for-ok'
+function getURL(appName: string) {
+  let host = appName || 'http://local.ft.com:3002'
 
-// function getURL(appName: string) {
-//   let host = appName || 'http://local.ft.com:3002'
+  if (!/:|\./.test(host)) host += '.herokuapp.com/__gtg'
 
-//   if (!/:|\./.test(host)) host += '.herokuapp.com/__gtg'
+  if (!/https?:\/\//.test(host)) host = 'http://' + host
 
-//   if (!/https?:\/\//.test(host)) host = 'http://' + host
+  return host
+}
 
-//   return host
-// }
+export default class GoodToGo extends Command {
+  static flags = {
+    app: flags.string({
+      char: 'a',
+      description: "Runs gtg ('good to go') checks for an app",
+      required: true
+    })
+  }
 
-// export default class GoodToGo extends Command {
-//   static flags = {
-//     app: flags.string({
-//       char: 'a',
-//       description: "Runs gtg ('good to go') checks for an app",
-//       required: true
-//     })
-//   }
+  async run() {
+    const { flags } = this.parse(GoodToGo)
 
-//   async run() {
-//     const { flags } = this.parse(GoodToGo)
+    const { app } = flags
 
-//     const { app } = flags
+    const url = getURL(app)
 
-//     const url = getURL(app)
-
-//     return waitForOk(url)
-//   }
-// }
+    return waitForOk(url)
+  }
+}
