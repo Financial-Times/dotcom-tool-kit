b4c4220effcc94f477bfe25b547accb18a4d543c
Leanne <leanne.cornish@ft.com>
2021-05-24 16:23:12 +0100

    changing from script to command to pass env variables

--- packages/circleci/src/index.ts
+++ packages/circleci/src/index.ts
@@ -1,19 +1,5 @@
-import { writeState } from '@dotcom-tool-kit/state'
+import CircleEnvVars from "./commands/envvars";
 
-const circleEnvs = {
-  repo_version: process.env.CIRCLE_SHA1,
-  repo_name: process.env.CIRCLE_PROJECT_REPONAME,
-  branch: process.env.CIRCLE_BRANCH
+export const commands = {
+    'circleci': CircleEnvVars
 }
-
-const run = () => {
-  for (const [key, val] of Object.entries(circleEnvs)) {
-   writeState(`ci`, key, val)
-  }
-
-}
-
-(async () => {
-  await run()
-})();
-
