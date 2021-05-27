8970e25d5cd2db9dc3efe1935b3139ee924f7d42
Leanne <leanne.cornish@ft.com>
2021-05-27 18:09:12 +0100

    changed readState to return an object

--- packages/state/src/index.ts
+++ packages/state/src/index.ts
@@ -2,23 +2,24 @@ import * as fs from 'fs'
 const target = process.env.INIT_CWD
 const stateFile = target ? `${target}/.toolkitstate.json` : '.toolkitstate.json'
 
-export function readState<T>(stage: T, items: string[]): string[] | T[] | null {
-
+export function readState<T>(stage: T, items: string[]): {[index: string]: string | number} | null {
+    let stateResults: {[index: string]:any} = {}
     if (fs.existsSync(stateFile)) {
       const readStateContent = JSON.parse(fs.readFileSync(stateFile, {encoding: 'utf-8'}))
-      return items.map(item => {
+      items.forEach(item => {
         try {
-          return readStateContent[stage][item]
+          stateResults[item] = readStateContent[stage][item]
         } catch {
           return null
         }
       })
+      return stateResults
     } 
   return null
 }
 
 
-export function writeState<T>(stage: string, item: string, value: T): string[] | T[] | null {
+export function writeState<T>(stage: string, item: string, value: T): {[index: string]:any} | null {
   if (fs.existsSync(stateFile)) {
     const readStateContent = JSON.parse(fs.readFileSync(stateFile, {encoding: 'utf-8'}))
     if (readStateContent[stage]) {
