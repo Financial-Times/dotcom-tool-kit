60f82efd1a1f463caa75b897265f0a569db69282
Leanne <leanne.cornish@ft.com>
2021-06-11 16:19:12 +0100

    moved circleci to script rather than command

--- packages/circleci/src/index.ts
+++ packages/circleci/src/index.ts
@@ -1,5 +1,15 @@
-import CircleEnvVars from "./commands/envvars";
+import { writeState } from '@dotcom-tool-kit/state'
 
-export const commands = {
-    'circleci': CircleEnvVars
+const envVars = {
+  branch: process.env.CIRCLE_BRANCH,
+  repo: process.env.CIRCLE_PR_REPONAME,
+  verison: process.env.CIRCLE_SHA1
 }
+
+;(async (): Promise<void> => {
+  for (const [key, val] of Object.entries(envVars)) {
+    console.log(key, val)
+    writeState(`ci`, key, val)
+  }
+  return
+})()
