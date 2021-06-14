c5254f2d3e7be55108cfeb6b4c860213b0bd4264
Kara Brightwell <kara@153.io>
2021-06-14 16:18:49 +0100

    error message for nonexistent lifecycle in command

--- packages/cli/src/commands/lifecycle.ts
+++ packages/cli/src/commands/lifecycle.ts
@@ -1,6 +1,7 @@
 import { runCommand } from '../'
 import { isConflict } from '../conflict'
 import { config } from '../config'
+import { ToolKitError } from '@dotcom-tool-kit/error'
 import type { Command } from '../command'
 
 export default class LifecycleCommand implements Command {
@@ -11,6 +12,19 @@ export default class LifecycleCommand implements Command {
   }
 
   async run(): Promise<void> {
+    const missingLifecycles = this.argv.filter(
+      id =>  !config.lifecycles[id]
+    )
+
+    if(missingLifecycles.length > 0) {
+      const error = new ToolKitError(`lifecycle events ${missingLifecycles} do not exist`)
+      error.details = `maybe you need to install a plugin to handle these lifecycles, or configure them in your Tool Kit configuration.
+
+lifecycle events that are available are:
+${Object.keys(config.lifecycles).map(id => `- ${id}`).join('\n')}`
+      throw error
+    }
+
     for (const id of this.argv) {
       const lifecycle = config.lifecycles[id]
 
