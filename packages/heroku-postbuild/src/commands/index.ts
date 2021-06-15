defaed0718858e26b1cf0cfbd369572d03d8b9b7
Kara Brightwell <kara@153.io>
2021-06-15 15:36:55 +0100

    delete heroku-postbuild package
    it's taken care of by the lifecycle-build package now

--- packages/heroku-postbuild/src/commands/index.ts
+++
@@ -1,7 +0,0 @@
-import HerokuPostbuild from './heroku/postbuild'
-import HerokuInstall from './heroku/install'
-
-export default {
-  'heroku:postbuild': HerokuPostbuild,
-  'heroku:install': HerokuInstall
-}
