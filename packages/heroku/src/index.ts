36eabc88685f901bff92924a824f2a487ecfeef7
Leanne <leanne.cornish@ft.com>
2021-06-30 11:57:28 +0100

    added HerokuCleanup to index

--- packages/heroku/src/index.ts
+++ packages/heroku/src/index.ts
@@ -1,9 +1,11 @@
 import HerokuProduction from './commands/production'
 import HerokuStaging from './commands/staging'
 import HerokuReview from './commands/review'
+import HerokuCleanup from './commands/cleanup'
 
 export const commands = {
   'heroku:production': HerokuProduction,
   'heroku:staging': HerokuStaging,
-  'heroku:review': HerokuReview
+  'heroku:review': HerokuReview,
+  'heroku:cleanup': HerokuCleanup
 }
