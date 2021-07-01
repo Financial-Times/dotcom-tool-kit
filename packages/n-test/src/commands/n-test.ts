19036fa29a3de2f41f6082d12d46a8e0f129dc57
Kara Brightwell <kara@153.io>
2021-07-01 11:15:12 +0100

    do it from the constructor instead

--- packages/n-test/src/commands/n-test.ts
+++ packages/n-test/src/commands/n-test.ts
@@ -7,14 +7,18 @@ export default class NTest extends Command {
 
   options: SmokeTestOptions = {}
 
-  async run(): Promise<void> {
+  constructor(argv: string[]) {
+    super(argv)
+
     const reviewState = readState('review')
 
     // if we've built a review app, test against that, not the app in the config
     if (reviewState) {
       this.options.host = `https://${reviewState.appId}.herokuapp.com`
     }
+  }
 
+  async run(): Promise<void> {
     const smokeTest = new SmokeTest(this.options)
     await smokeTest.run()
   }
