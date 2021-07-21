5801f104421f4844bb3544b91651b758a3f77e6e
Leanne <leanne.cornish@ft.com>
2021-07-21 16:11:53 +0100

    corrected repo env variable

--- packages/circleci/src/index.ts
+++ packages/circleci/src/index.ts
@@ -2,12 +2,12 @@ import { writeState } from '@dotcom-tool-kit/state'
 
 const envVars = {
   branch: process.env.CIRCLE_BRANCH,
-  repo: process.env.CIRCLE_PR_REPONAME,
+  repo: process.env.CIRCLE_PROJECT_REPONAME,
   version: process.env.CIRCLE_SHA1
 }
 
 function pluginInit() {
-  if(process.env.CIRCLECI) {
+  if (process.env.CIRCLECI) {
     console.log(`writing circle ci environment variables to state...`)
     writeState('ci', envVars)
   }
