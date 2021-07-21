dd70db38e8d9bbb6bf52b88468ee0723efc902f2
Kara Brightwell <kara@153.io>
2021-07-21 14:38:13 +0100

    correct heroku auth token env var name

--- packages/heroku/src/herokuClient.ts
+++ packages/heroku/src/herokuClient.ts
@@ -1,5 +1,5 @@
 import Heroku from 'heroku-client'
 
-const HEROKU_API_TOKEN = process.env.HEROKU_API_TOKEN
+const HEROKU_AUTH_TOKEN = process.env.HEROKU_AUTH_TOKEN
 
-export default new Heroku({ token: HEROKU_API_TOKEN })
+export default new Heroku({ token: HEROKU_AUTH_TOKEN })
