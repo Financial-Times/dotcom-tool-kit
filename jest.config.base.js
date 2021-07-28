1c64f99f504c50a7ca6af69b108ad2dff5b8ae05
Kara Brightwell <kara@153.io>
2021-07-28 16:22:16 +0100

    explicitly set base tsconfig for jest

--- jest.config.base.js
+++ jest.config.base.js
@@ -2,5 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/lib/', '/test/files'],
-  clearMocks: true
+  clearMocks: true,
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.settings.json'
+    }
+  }
 }
