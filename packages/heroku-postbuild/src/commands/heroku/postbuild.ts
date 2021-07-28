fc50046bbb4cf7b6a643a1516c3fa798e55cb9ff
Kara Brightwell <kara@153.io>
2021-07-28 16:45:51 +0100

    delete a package to test it

--- packages/heroku-postbuild/src/commands/heroku/postbuild.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from '@dotcom-tool-kit/command'
-
-export default class HerokuPostbuild extends Command {
-  static description = ''
-
-  async run(): Promise<void> {
-    console.log('heroku postbuild')
-  }
-}
