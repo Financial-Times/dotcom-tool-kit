b1a0a8630cd4454c06a4fd866f263dc66f7068a6
Kara Brightwell <kara@153.io>
2021-07-29 08:31:29 +0100

    don't print hashes

--- scripts/clean-up-packages.sh
+++ scripts/clean-up-packages.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-readonly oldBranch="$1"
-readonly newBranch="$2"
 # third argument for the hook is a flag telling us if this was a branch move or a file checkout
 readonly isBranchMove="$3"
 
 if [ "$isBranchMove" = "1" ]; then
-	echo "moved from $oldBranch to $newBranch, cleaning up zombie packages"
+	echo "changed branches, cleaning up zombie packages..."
 	for package in packages/*; do
 		if ! [ -f "$package/package.json" ]; then
 			echo "deleting $package"
