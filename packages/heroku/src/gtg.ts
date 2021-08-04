e928f3ccb5a98e3c4ee00f1ce1104e6b3f66effc
Leanne <leanne.cornish@ft.com>
2021-08-04 18:45:21 +0100

    adding logging to check for writing to state in gtg

--- packages/heroku/src/gtg.ts
+++ packages/heroku/src/gtg.ts
@@ -1,7 +1,7 @@
 import heroku from './herokuClient'
 import type { HerokuApiResGetGtg } from 'heroku-client'
 import waitForOk from '@dotcom-tool-kit/wait-for-ok'
-import { State, writeState } from '@dotcom-tool-kit/state'
+import { State, writeState, readState } from '@dotcom-tool-kit/state'
 
 export default async function gtg(appIdName: string, environment: keyof State, id = true): Promise<void> {
   let appName = appIdName
@@ -12,6 +12,9 @@ export default async function gtg(appIdName: string, environment: keyof State, i
   }
   //save name to state file
   writeState(environment, { appName })
+
+  const reviewState = readState('review')
+  console.log(`review app name written to state: ${reviewState?.appName}`)
 
   const url = `https://${appName}.herokuapp.com/__gtg`
 
