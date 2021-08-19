dc623b99780ded5e295b0631ddfcf5625fa3dc75
Kara Brightwell <kara@153.io>
2021-08-19 10:04:04 +0100

    add example hook descriptions
    Co-authored-by: leannecornish-ft <leannecornish-ft@users.noreply.github.com>
    Co-authored-by: Ivo Murrell <ivomurrell@users.noreply.github.com>

--- packages/npm/src/index.ts
+++ packages/npm/src/index.ts
@@ -1,11 +1,15 @@
 import { PackageJsonHook } from '@dotcom-tool-kit/package-json-hook'
 
 class BuildLocal extends PackageJsonHook {
+  static description = 'hook for `npm run build`, for building an app locally'
+
   script = 'build'
   command = 'dotcom-tool-kit build:local'
 }
 
 class TestLocal extends PackageJsonHook {
+  static description = 'hook for `npm run test`, for running tests locally'
+
   script = 'test'
   command = 'dotcom-tool-kit test:local'
 }
