255e332a098271821f2ebf18f4d5e5102c81d055
Leanne <leanne.cornish@ft.com>
2021-08-24 14:16:57 +0100

    add npm prune as a clean-up task

--- packages/heroku/src/tasks/cleanup.ts
+++ packages/heroku/src/tasks/cleanup.ts
@@ -1,20 +1,15 @@
 import { Task } from '@dotcom-tool-kit/task'
-import { readState } from '@dotcom-tool-kit/state'
-import scaleDyno from '../scaleDyno'
 import { ToolKitError } from '@dotcom-tool-kit/error'
+import exec from '@actions/exec'
 
 export default class HerokuCleanup extends Task {
   static description = ''
 
   async run(): Promise<void> {
-    //scale down staging
-    const state = readState('staging')
-
-    if (!state) {
-      throw new ToolKitError('Could not find state for staging, check that deploy:staging ran successfully')
+    try {
+      await exec.exec(`npm prune --production`)
+    } catch {
+      throw new ToolKitError(`Unable to prune dev dependencies`)
     }
-
-    const appName = state.appName
-    await scaleDyno(appName, 0)
   }
 }
