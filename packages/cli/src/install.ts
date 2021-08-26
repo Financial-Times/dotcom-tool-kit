2dd91245566494297f7ad38aa82a1de11ef98851
Ivo Murrell <ivo.murrell@ft.com>
2021-08-26 15:04:13 +0100

    Initial draft of automatic CircleCI installation
    Switch to from yawn-yaml to the underlying js-yaml dependency. The
    higher level abstractions were making mutating the config awkward, it
    had quite a few bugs, and we don't care so much about preserving the
    comments and indentation when it's an automatically generated config
    that is written out.
    
    For now just the job names are written out, though we will also want to
    include the filters and requires properties too.

--- packages/cli/src/install.ts
+++ packages/cli/src/install.ts
@@ -7,17 +7,22 @@ const isRejected = (result: PromiseSettledResult<unknown>): result is PromiseRej
 export default async function installHooks(): Promise<void> {
   const config = await loadConfig({ checkInstall: false })
 
-  const results = await Promise.allSettled(
-    Object.values(config.hooks).map(async (Hook) => {
-      const hook = new Hook()
+  const tasks = Object.values(config.hooks).map((Hook) => async () => {
+    const hook = new Hook()
 
-      if (!(await hook.check())) {
-        await hook.install()
-      }
-    })
-  )
+    if (!(await hook.check())) {
+      await hook.install()
+    }
+  })
 
-  const errors = results.filter(isRejected).map((result) => result.reason as Error)
+  const errors: Error[] = []
+  for (const task of tasks) {
+    try {
+      await task()
+    } catch (err) {
+      errors.push(err)
+    }
+  }
 
   if (errors.length) {
     const error = new ToolKitError('could not automatically install hooks:')
