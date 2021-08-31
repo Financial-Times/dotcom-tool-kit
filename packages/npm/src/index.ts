51fd5cb0cbf4d6a6d32e3a8fbe34016b34009e6d
Leanne <leanne.cornish@ft.com>
2021-08-31 17:04:40 +0100

    missed the npm hooks...

--- packages/npm/src/index.ts
+++ packages/npm/src/index.ts
@@ -5,14 +5,14 @@ class BuildLocal extends PackageJsonHook {
   static description = 'hook for `npm run build`, for building an app locally'
 
   script = 'build'
-  command = 'dotcom-tool-kit build:local'
+  hook = 'build:local'
 }
 
 class TestLocal extends PackageJsonHook {
   static description = 'hook for `npm run test`, for running tests locally'
 
   script = 'test'
-  command = 'dotcom-tool-kit test:local'
+  hook = 'test:local'
 }
 
 export { PackageJsonHook }
