7e0cdb9d8a3c6aed3543d519fb1bcdc4271e0c73
Leanne <leanne.cornish@ft.com>
2021-09-06 16:20:30 +0100

    changed getting review app to including when apps are creating

--- packages/heroku/src/getHerokuReviewApp.ts
+++ packages/heroku/src/getHerokuReviewApp.ts
@@ -2,6 +2,7 @@ import heroku from './herokuClient'
 import type { HerokuApiResGetReview } from 'heroku-client'
 import { readState } from '@dotcom-tool-kit/state'
 import { ToolKitError } from '@dotcom-tool-kit/error'
+import repeatedCheckForSuccessStatus from './repeatedCheckForSuccessStatus'
 
 export default async function getHerokuReviewApp(pipelineId: string): Promise<string | undefined> {
   const state = readState('ci')
@@ -12,12 +13,19 @@ export default async function getHerokuReviewApp(pipelineId: string): Promise<st
 
   const branch = state.branch
   const reviewApps: HerokuApiResGetReview[] = await heroku.get(`/pipelines/${pipelineId}/review-apps`)
+  let creatingAppId
 
   const reviewApp = reviewApps.find(
     (instance: { app: { id: string }; branch: string; status: string }): boolean => {
+      if (instance.branch === branch && instance.status === 'creating') {
+        creatingAppId = instance.app.id
+      }
       return instance.branch === branch && instance.status === 'created'
     }
   )
+  if (creatingAppId) {
+    await repeatedCheckForSuccessStatus(creatingAppId)
+  }
 
   return reviewApp?.app?.id
 }
