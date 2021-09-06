19dd066727f645a4e3f33661afbfb2827d97253a
Leanne <leanne.cornish@ft.com>
2021-09-06 14:44:10 +0100

    typo in dyno patch, added extra logging

--- packages/heroku/src/scaleDyno.ts
+++ packages/heroku/src/scaleDyno.ts
@@ -2,7 +2,9 @@ import heroku from './herokuClient'
 import { ToolKitError } from '@dotcom-tool-kit/error'
 
 export default async function scaleDyno(appName: string, quantity: number, type = 'web'): Promise<void> {
-  const appFormation = await heroku.patch(`/apps/ft-${appName}/formation`, {
+  console.log(`scaling dyno for ${appName}...`)
+
+  const appFormation = await heroku.patch(`/apps/${appName}/formation`, {
     updates: [
       {
         quantity: quantity,
@@ -14,6 +16,6 @@ export default async function scaleDyno(appName: string, quantity: number, type 
   if (appFormation.quantity === quantity && appFormation.type === type) {
     return
   } else {
-    throw new ToolKitError(`Something went wrong with scaling the dyno`)
+    throw new ToolKitError(`something went wrong with scaling the dyno`)
   }
 }
