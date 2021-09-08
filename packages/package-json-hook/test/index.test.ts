436f22615fec8301b1b086d534fe3aba1f5806a3
Kara Brightwell <kara@153.io>
2021-09-08 11:44:05 +0100

    add tests for package json hook install

--- packages/package-json-hook/test/index.test.ts
+++ packages/package-json-hook/test/index.test.ts
@@ -1,20 +1,21 @@
 import { describe, it, expect } from '@jest/globals'
 import * as path from 'path'
+import * as fs from 'fs/promises'
 import { PackageJsonHook } from '../src'
 
 describe('package.json hook', () => {
+  class TestHook extends PackageJsonHook {
+    script = 'test-hook'
+    hook = 'test:hook'
+  }
+
+  const originalDir = process.cwd()
+
+  afterEach(() => {
+    process.chdir(originalDir)
+  })
+
   describe('check', () => {
-    class TestHook extends PackageJsonHook {
-      script = 'test-hook'
-      hook = 'test:hook'
-    }
-
-    const originalDir = process.cwd()
-
-    afterEach(() => {
-      process.chdir(originalDir)
-    })
-
     it('should return true when package.json has hook call in script', async () => {
       process.chdir(path.join(__dirname, 'files', 'with-hook'))
       const hook = new TestHook()
@@ -34,6 +35,44 @@ describe('package.json hook', () => {
       const hook = new TestHook()
 
       expect(await hook.check()).toBeFalsy()
+    })
+  })
+
+  describe('install', () => {
+    it(`should add script when it doesn't exist`, async () => {
+      const base = path.join(__dirname, 'files', 'without-hook')
+
+      const pkgPath = path.join(base, 'package.json')
+      const originalJson = await fs.readFile(pkgPath, 'utf-8')
+
+      process.chdir(base)
+
+      const hook = new TestHook()
+      await hook.install()
+
+      const packageJson = JSON.parse(await fs.readFile(path.join(base, 'package.json'), 'utf-8'))
+
+      expect(packageJson).toHaveProperty(['scripts', 'test-hook'], 'dotcom-tool-kit test:hook')
+
+      await fs.writeFile(pkgPath, originalJson)
+    })
+
+    it('should prepend hook to a call with an existing hook', async () => {
+      const base = path.join(__dirname, 'files', 'existing-hook')
+
+      const pkgPath = path.join(base, 'package.json')
+      const originalJson = await fs.readFile(pkgPath, 'utf-8')
+
+      process.chdir(base)
+
+      const hook = new TestHook()
+      await hook.install()
+
+      const packageJson = JSON.parse(await fs.readFile(path.join(base, 'package.json'), 'utf-8'))
+
+      expect(packageJson).toHaveProperty(['scripts', 'test-hook'], 'dotcom-tool-kit test:hook another:hook')
+
+      await fs.writeFile(pkgPath, originalJson)
     })
   })
 })
