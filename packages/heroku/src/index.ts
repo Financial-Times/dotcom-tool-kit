acb2fe6b54d97120b09a6ac1a2d15d14cd8e1b9c
Leanne <leanne.cornish@ft.com>
2021-09-27 11:32:37 +0100

    swap order that hooks are run to allow build before upload

--- packages/heroku/src/index.ts
+++ packages/heroku/src/index.ts
@@ -18,16 +18,11 @@ class ReleaseRemote extends PackageJsonHook {
   script = 'heroku-postbuild'
   hook = 'release:remote'
 }
-class ReleaseDeploy extends PackageJsonHook {
-  script = 'heroku-postbuild'
-  hook = 'release:deploy'
-}
 
 export const hooks = {
   'cleanup:remote': CleanupRemote,
-  'release:deploy': ReleaseDeploy,
-  'build:remote': BuildRemote,
-  'release:remote': ReleaseRemote
+  'release:remote': ReleaseRemote,
+  'build:remote': BuildRemote
 }
 
 export const tasks = [HerokuProduction, HerokuStaging, HerokuReview, HerokuTeardown]
