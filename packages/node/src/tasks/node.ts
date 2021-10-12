9a224fd8cc38055b4bafb5060d3234c9c90668f3
Kara Brightwell <kara@153.io>
2021-10-12 16:53:13 +0100

    get local env from vault for node plugin

--- packages/node/src/tasks/node.ts
+++ packages/node/src/tasks/node.ts
@@ -2,6 +2,7 @@ import { Task } from '@dotcom-tool-kit/task'
 import { NodeOptions, NodeSchema } from '@dotcom-tool-kit/types/lib/schema/node'
 import { ToolKitError } from '@dotcom-tool-kit/error'
 import { fork } from 'child_process'
+import { VaultEnvVars } from '@dotcom-tool-kit/vault'
 
 export default class Node extends Task<typeof NodeSchema> {
   static description = ''
@@ -12,6 +13,11 @@ export default class Node extends Task<typeof NodeSchema> {
 
   async run(): Promise<void> {
     const { entry } = this.options
+    const vault = new VaultEnvVars({
+      environment: 'development'
+    })
+
+    const vaultEnv = await vault.get()
 
     if (!entry) {
       const error = new ToolKitError('the Node tasks requires an `entry` option')
@@ -24,8 +30,9 @@ export default class Node extends Task<typeof NodeSchema> {
       // TODO arguments?
       {
         env: {
+          ...vaultEnv,
           ...process.env
-          // TODO: vault, PORT
+          // TODO: PORT
         },
         stdio: 'inherit'
       }
