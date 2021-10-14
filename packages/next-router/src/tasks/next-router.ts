85abd2c5373fe5e140ce683a7f2e2510007971b8
Kara Brightwell <kara@153.io>
2021-10-14 15:27:07 +0100

    get app name for router from options

--- packages/next-router/src/tasks/next-router.ts
+++ packages/next-router/src/tasks/next-router.ts
@@ -4,11 +4,23 @@ import { register } from 'ft-next-router'
 import { readState } from '@dotcom-tool-kit/state'
 import { ToolKitError } from '@dotcom-tool-kit/error'
 import { fork } from 'child_process'
+import { NextRouterSchema } from '@dotcom-tool-kit/types/lib/schema/next-router'
 
-export default class NextRouter extends Task {
+export default class NextRouter extends Task<typeof NextRouterSchema> {
   static description = ''
 
   async run(): Promise<void> {
+    if (!this.options.appName) {
+      const error = new ToolKitError('your app name must be configured to use next-router')
+      error.details = `this should be the same as its "name" field in next-service-registry. configure it in your .toolkitrc.yml, e.g.:
+
+options:
+  '@dotcom-tool-kit/next-router':
+    appName: article`
+
+      throw error
+    }
+
     const vault = new VaultEnvVars({
       environment: 'development',
       vaultPath: {
@@ -46,7 +58,6 @@ export default class NextRouter extends Task {
       throw error
     }
 
-    const service = 'static' // TODO where from
-    await register({ service, port: local.port })
+    await register({ service: this.options.appName, port: local.port })
   }
 }
