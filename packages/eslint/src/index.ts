af6048e2e44b9c3a5ea84a9c46bb9864b23723c3
Ivo Murrell <ivo.murrell@ft.com>
2021-10-20 16:06:10 +0100

    Add lint-staged hook to eslint plugin

--- packages/eslint/src/index.ts
+++ packages/eslint/src/index.ts
@@ -1,3 +1,16 @@
+import { LintStagedHook } from '@dotcom-tool-kit/lint-staged'
 import Eslint from './tasks/eslint'
+import { getOptions } from '@dotcom-tool-kit/options'
 
 export const tasks = [Eslint]
+
+class TestStaged extends LintStagedHook {
+  static description = 'format prettier'
+
+  glob = getOptions('@dotcom-tool-kit/eslint')?.lintStagedGlob ?? '**/*.js'
+  hook = 'test:staged'
+}
+
+export const hooks = {
+  'test:staged': TestStaged
+}
--- packages/types/src/schema/eslint.ts
+++ packages/types/src/schema/eslint.ts
@@ -2,7 +2,8 @@ import { SchemaOutput } from '../schema'
 
 export const ESLintSchema = {
   files: 'array.string',
-  config: 'record.unknown?'
+  config: 'record.unknown?',
+  lintStagedGlob: 'string?'
 } as const
 export type ESLintOptions = SchemaOutput<typeof ESLintSchema>
 
