bef57b229373095194b81d715df99cee0b2a6c02
Ivo Murrell <ivo.murrell@ft.com>
2021-11-08 17:33:52 +0000

    Add integration test that plugins are well-formed

---
+++ packages/hook/test/plugin.test.js
@@ -0,0 +1,39 @@
+/*
+ * File must be JavaScript as ts-jest transform breaks instanceof Hook test in
+ * instantiatePlugin function, apparently comparing two different versions of
+ * the Hook constructor when going up the plugin's prototype chain.
+ */
+const { describe, it } = require('@jest/globals')
+const path = require('path')
+const { instantiatePlugin } = require('../lib/types')
+
+// TODO: come up with a better way to get list of plugins to test – perhaps by
+// separating plugins (like prettier) from libraries (like error) and apps (like
+// cli) by putting them into different directories in the monorepo?
+describe.each([
+  'babel',
+  'circleci',
+  'circleci-heroku',
+  'eslint',
+  'frontend-app',
+  'heroku',
+  'husky-hook',
+  'husky-npm',
+  'lint-staged',
+  'lint-staged-npm',
+  'mocha',
+  'next-router',
+  'node',
+  'npm',
+  'n-test',
+  'package-json-hook',
+  'prettier',
+  'upload-assets-to-s3',
+  'webpack'
+])('%s integration test', (plugin) => {
+  const packagePath = path.join(__dirname, `../../${plugin}`)
+  it('should be a valid plugin', () => {
+    const pluginPackage = require(packagePath)
+    instantiatePlugin(pluginPackage)
+  })
+})
