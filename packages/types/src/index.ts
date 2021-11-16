bb054a4967b9eebf5cd5ea03181df79e473e7d00
Ivo Murrell <ivo.murrell@ft.com>
2021-11-16 17:46:30 +0000

    Use Plugin type in Task class
    It's possible now that the Plugin type is defined in the same place as
    the Task class.

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -5,10 +5,7 @@ import { Schema, SchemaOutput } from './schema'
 
 export abstract class Task<O extends Schema = Record<string, never>> {
   static description: string
-  // plugin is set by the CLI package to a type it defines.
-  // we could extract it into a package, but for now:
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  static plugin?: any
+  static plugin?: Plugin
   static id?: string
 
   static defaultOptions: Record<string, unknown> = {}
