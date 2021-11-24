b0808a152b64a1515a22308ce8f83a530f451399
Ivo Murrell <ivo.murrell@ft.com>
2021-11-24 15:45:27 +0000

    Rename packages directory to core
    Has a clearer meaning than the generic packages name.
    
    sandbox doesn't really belong in the core directory but fixing
    that (possibly by moving it into a test suite) is beyond the scope of
    this commit.

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ const base = require('./jest.config.base')
 
 module.exports = {
   ...base,
-  projects: ['<rootDir>/packages/*', '<rootDir>/plugins/*', '<rootDir>/lib/*']
+  projects: ['<rootDir>/core/*', '<rootDir>/plugins/*', '<rootDir>/lib/*']
 }
