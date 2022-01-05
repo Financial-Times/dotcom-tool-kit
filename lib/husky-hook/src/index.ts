7c9e384adb04cb577e3d2f358fbae10ec295e513
Kara Brightwell <kara@153.io>
2022-01-05 17:21:25 +0000

    fix husky config format

--- lib/husky-hook/src/index.ts
+++ lib/husky-hook/src/index.ts
@@ -1,7 +1,9 @@
 import { PackageJsonHelper } from '@dotcom-tool-kit/hook'
 
 type HuskyHooks = {
-  [hook: string]: string
+  hooks: {
+    [hook: string]: string
+  }
 }
 
 export abstract class HuskyHook extends PackageJsonHelper {
@@ -10,11 +12,11 @@ export abstract class HuskyHook extends PackageJsonHelper {
   async install(): Promise<void> {
     let command = `dotcom-tool-kit ${this.hook}`
     const huskyHooks = this.packageJson.getField<HuskyHooks>(this.field) || {}
-    const existingCommand = huskyHooks[this.key]
+    const existingCommand = huskyHooks.hooks[this.key]
     if (existingCommand?.startsWith('dotcom-tool-kit ')) {
       command = command.concat(existingCommand.replace('dotcom-tool-kit', ''))
     }
-    huskyHooks[this.key] = command
+    huskyHooks.hooks[this.key] = command
     this.packageJson.setField(this.field, huskyHooks)
 
     this.packageJson.writeChanges()
