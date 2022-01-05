efc5fd822dacf7c829a69ad742ba8ccb4824ba39
Kara Brightwell <kara@153.io>
2022-01-05 17:26:48 +0000

    set husky.hooks if it doesn't exist

--- lib/husky-hook/src/index.ts
+++ lib/husky-hook/src/index.ts
@@ -1,7 +1,7 @@
 import { PackageJsonHelper } from '@dotcom-tool-kit/hook'
 
 type HuskyHooks = {
-  hooks: {
+  hooks?: {
     [hook: string]: string
   }
 }
@@ -11,14 +11,22 @@ export abstract class HuskyHook extends PackageJsonHelper {
 
   async install(): Promise<void> {
     let command = `dotcom-tool-kit ${this.hook}`
+
     const huskyHooks = this.packageJson.getField<HuskyHooks>(this.field) || {}
+
+    if(!huskyHooks.hooks) {
+      huskyHooks.hooks = {}
+    }
+
     const existingCommand = huskyHooks.hooks[this.key]
+
     if (existingCommand?.startsWith('dotcom-tool-kit ')) {
       command = command.concat(existingCommand.replace('dotcom-tool-kit', ''))
     }
-    huskyHooks.hooks[this.key] = command
-    this.packageJson.setField(this.field, huskyHooks)
 
+    huskyHooks.hooks[this.key] = command
+
+    this.packageJson.setField(this.field, huskyHooks)
     this.packageJson.writeChanges()
   }
 }
