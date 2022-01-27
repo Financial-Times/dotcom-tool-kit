b48fe73dd6de46f997d3156c53832120f3e44b00
Ivo Murrell <ivo.murrell@ft.com>
2022-01-27 15:47:06 +0000

    Increase default logging verbosity in CI runs

--- lib/logger/src/logger.ts
+++ lib/logger/src/logger.ts
@@ -3,7 +3,7 @@ import { format } from './format'
 import { consoleTransport } from './transports'
 
 export const rootLogger: winston.Logger = winston.createLogger({
-  level: process.env.LOG_LEVEL,
+  level: process.env.LOG_LEVEL ?? (process.env.CIRCLECI ? 'verbose' : 'info'),
   format,
   transports: [consoleTransport]
 })
