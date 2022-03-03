082199e275e2d596ca8a213c5dae7661c5943896
Leanne <leanne.cornish@ft.com>
2022-03-03 15:50:33 +0000

    updating staging task to build on each run

--- plugins/heroku/src/tasks/staging.ts
+++ plugins/heroku/src/tasks/staging.ts
@@ -3,10 +3,13 @@ import { ToolKitError } from '@dotcom-tool-kit/error'
 import { styles } from '@dotcom-tool-kit/logger'
 import { getHerokuStagingApp } from '../getHerokuStagingApp'
 import { setAppConfigVars } from '../setConfigVars'
+import { createBuild } from '../createBuild'
+import { repeatedCheckForBuildSuccess } from '../repeatedCheckForBuildSuccess'
 import { scaleDyno } from '../scaleDyno'
 import { gtg } from '../gtg'
 import { getPipelineCouplings } from '../getPipelineCouplings'
 import { HerokuSchema } from '@dotcom-tool-kit/types/lib/schema/heroku'
+import { setStagingSlug } from '../setStagingSlug'
 
 export default class HerokuStaging extends Task<typeof HerokuSchema> {
   static description = ''
@@ -31,10 +34,22 @@ options:
       await getPipelineCouplings(this.logger, this.options.pipeline)
 
       this.logger.verbose(`restrieving staging app details...`)
-      const appName = await getHerokuStagingApp(this.logger)
+      const appName = await getHerokuStagingApp()
 
       // setting config vars on staging from the vault production directory
       await setAppConfigVars(this.logger, appName, 'production', this.options.systemCode)
+
+      // create build from latest commit, even on no change
+      const buildDetails = await createBuild(this.logger, appName)
+
+      // wait for build to complete
+      if (buildDetails.slug === null) {
+        const id = await repeatedCheckForBuildSuccess(this.logger, appName, buildDetails.id)
+        buildDetails.slug = {id}
+      }
+
+      // apply build
+      await setStagingSlug(this.logger, appName, buildDetails.slug.id)
 
       //scale up staging
       await scaleDyno(this.logger, appName, 1)
