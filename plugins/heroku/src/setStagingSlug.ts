6be6607db1d1aa2defc0882ebc041602dcdcf22f
Leanne <leanne.cornish@ft.com>
2022-03-03 15:30:37 +0000

    setting staging slug once build succeeded

---
+++ plugins/heroku/src/setStagingSlug.ts
@@ -0,0 +1,26 @@
+import type { Logger } from 'winston'
+import heroku from './herokuClient'
+import { ToolKitError } from '@dotcom-tool-kit/error'
+import { HerokuApiResPost } from 'heroku-client'
+
+async function setStagingSlug(logger: Logger, appName: string, slug: string): Promise<void> {
+	try{
+		logger.info(`updating slug id ${slug} on ${appName}`)
+	  
+		await heroku.post<HerokuApiResPost>(`/apps/${appName}/releases`, {
+			body: {
+			slug
+			}
+		})
+	} catch(err) {
+        const error = new ToolKitError(
+          `there was an error with setting the slug on ${appName}`
+        )
+		if (err instanceof Error) {
+			error.details = err.message
+		  }
+		throw Error
+      }
+}
+
+export { setStagingSlug }
