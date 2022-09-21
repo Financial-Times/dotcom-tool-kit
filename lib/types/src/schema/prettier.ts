2a15eab2432cf9b0464bc3c4023f59f136350059
Kara Brightwell <kara@153.io>
2022-09-21 12:12:10 +0100

    fix: prettier plugin respects .prettierignore

--- lib/types/src/schema/prettier.ts
+++ lib/types/src/schema/prettier.ts
@@ -3,6 +3,7 @@ import { SchemaOutput } from '../schema'
 export const PrettierSchema = {
   files: 'array.string',
   configFile: 'string?',
+  ignoreFile: 'string?',
   configOptions: 'record.unknown?'
 } as const
 export type PrettierOptions = SchemaOutput<typeof PrettierSchema>
