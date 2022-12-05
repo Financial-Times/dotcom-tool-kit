6f48e8c5d7cb39296222487b34617f491fc4c20e
Ivo Murrell <ivo.murrell@ft.com>
2022-12-05 18:13:03 +0000

    fix(circleci-heroku): run provision step even for nori/renovate builds
    I think this was always an oversight rather than something we can only
    do now that we've made other improvements to the code. The job now
    requires the 'waiting-for-approval' job to have been run first, though
    this only runs for nori/renovate branches. If that job is filtered out
    then this job will instead simply run after 'tool-kit/setup' is
    completed. n-gage would previously create a different workflow for
    nori/renonate, with the 'waiting-for-approval' job required before
    anything else was run. Tool Kit's approach is much simpler and reduces
    duplication.

--- plugins/circleci-heroku/src/index.ts
+++ plugins/circleci-heroku/src/index.ts
@@ -4,8 +4,8 @@ import { TestCI } from '@dotcom-tool-kit/circleci/lib/index'
 export class DeployReview extends CircleCiConfigHook {
   job = 'tool-kit/heroku-provision'
   jobOptions = {
-    requires: ['tool-kit/setup'],
-    filters: { branches: { ignore: '/(^renovate-.*|^nori/.*|^main)/' } }
+    requires: ['tool-kit/setup', 'waiting-for-approval'],
+    filters: { branches: { ignore: 'main' } }
   }
 }
 
