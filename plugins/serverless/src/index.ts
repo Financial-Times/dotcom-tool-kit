6f49aaa80bb315e5dfd11068a21cb1d3e52ef36a
Ivo Murrell <ivo.murrell@ft.com>
2023-02-20 20:48:52 +0000

    feat(serverless): define ServerlessProvision task

--- plugins/serverless/src/index.ts
+++ plugins/serverless/src/index.ts
@@ -1,3 +1,4 @@
+import ServerlessProvision from './tasks/provision'
 import ServerlessRun from './tasks/run'
 
-export const tasks = [ServerlessRun]
+export const tasks = [ServerlessRun, ServerlessProvision]
