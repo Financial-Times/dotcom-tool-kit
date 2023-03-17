f2e9cc0b45d79c26c5ee5ec248fe44f073900835
Ivo Murrell <ivo.murrell@ft.com>
2023-03-17 20:14:34 +0000

    fix(types): make vault options optional
    In theory, these fields should be required as Vault won't work without
    them, but not every app that pulls in the Vault plugin actually needs to
    use Vault, e.g., an app that uses the nodemon plugin with the useVault
    option set to false.

--- lib/types/src/schema/vault.ts
+++ lib/types/src/schema/vault.ts
@@ -1,9 +1,15 @@
 import { z } from 'zod'
 
-export const VaultSchema = z.object({
-  team: z.string(),
-  app: z.string()
-})
+// In theory, these fields should be required as Vault won't work without them,
+// but not every app that pulls in the Vault plugin actually needs to use
+// Vault, e.g., an app that uses the `nodemon` plugin with the `useVault`
+// option set to false.
+export const VaultSchema = z
+  .object({
+    team: z.string(),
+    app: z.string()
+  })
+  .partial()
 export type VaultOptions = z.infer<typeof VaultSchema>
 
 export const Schema = VaultSchema
