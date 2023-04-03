7277b58be3583d63f41485bc1ad59566c8dfbfbf
Ivo Murrell <ivo.murrell@ft.com>
2023-04-03 17:13:34 +0100

    fix(serverless): export ServerlessDeploy task

--- plugins/serverless/src/index.ts
+++ plugins/serverless/src/index.ts
@@ -1,4 +1,5 @@
+import ServerlessDeploy from './tasks/deploy'
 import ServerlessProvision from './tasks/provision'
 import ServerlessRun from './tasks/run'
 
-export const tasks = [ServerlessRun, ServerlessProvision]
+export const tasks = [ServerlessRun, ServerlessDeploy, ServerlessProvision]
