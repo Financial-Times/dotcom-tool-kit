353e96cde61b657248d16440ef495ae491f8542b
Ivo Murrell <ivo.murrell@ft.com>
2023-09-01 21:18:31 +0100

    feat(doppler): fall back to Vault if Doppler fails
    This is a temporary measure to ease the migration to Doppler and avoid
    any breaking changes. We will remove all Vault support once people have
    migrated over to Doppler.

--- lib/doppler/src/index.ts
+++ lib/doppler/src/index.ts
@@ -3,18 +3,35 @@ import { spawn } from 'child_process'
 import type { Logger } from 'winston'
 
 import { ToolKitError } from '@dotcom-tool-kit/error'
-import { waitOnExit } from '@dotcom-tool-kit/logger'
+import { styles, waitOnExit } from '@dotcom-tool-kit/logger'
 import { getOptions } from '@dotcom-tool-kit/options'
-import type { DopplerOptions } from '@dotcom-tool-kit/types/lib/schema/doppler'
+import * as Vault from '@dotcom-tool-kit/vault'
+import type { DopplerOptions as ConfiguredDopplerOptions } from '@dotcom-tool-kit/types/lib/schema/doppler'
 
 export type Environment = 'prd' | 'ci' | 'dev'
+
+type DopplerOptions = Required<ConfiguredDopplerOptions>
+
+const dopplerEnvToVaultMap: Record<Environment, Vault.Environment> = {
+  prd: 'production',
+  ci: 'continuous-integration',
+  dev: 'development'
+}
+
+let hasLoggedMigrationWarning = false
 
 export class DopplerEnvVars {
   options: DopplerOptions
 
   constructor(private logger: Logger, public environment: Environment, options?: DopplerOptions) {
-    const dopplerOptions = options ?? getOptions('@dotcom-tool-kit/doppler')
-    if (!dopplerOptions || !('project' in dopplerOptions)) {
+    let dopplerOptions = options ?? getOptions('@dotcom-tool-kit/doppler')
+    if (!(dopplerOptions && dopplerOptions.project)) {
+      // HACK:20230829:IM check the project passed to the Vault options too so
+      // that projects don't have to make any changes to their Tool Kit config
+      logger.warn('Doppler options not found so falling back to Vault options')
+      dopplerOptions = { project: getOptions('@dotcom-tool-kit/vault')?.app }
+    }
+    if (!(dopplerOptions && dopplerOptions.project)) {
       const error = new ToolKitError('Doppler options not found in your Tool Kit configuration')
       error.details = `"project" is needed to get your app's secrets from doppler, e.g.
         options:
@@ -23,28 +40,58 @@ export class DopplerEnvVars {
             `
       throw error
     }
-    this.options = dopplerOptions
+    // TypeScript can't seem to detect that the project field is no longer
+    // partial unless it is accessed directly
+    this.options = { project: dopplerOptions.project }
   }
 
   async get(): Promise<Record<string, string>> {
+    let errorMsg = ''
     try {
+      let secrets = ''
       const dopplerChild = spawn('doppler', [
         'secrets',
         'download',
         '--no-file',
         '--project',
-        this.options.project!,
+        this.options.project,
         '--config',
         this.environment
       ])
-      let secrets = ''
-      dopplerChild.stdout.on('data', (data) => (secrets += data))
+      dopplerChild.stdout?.on('data', (data) => (secrets += data))
+      dopplerChild.stderr?.on('data', (data) => (errorMsg += data))
       await waitOnExit('doppler', dopplerChild)
-      return JSON.parse(secrets)
-    } catch {
-      const error = new ToolKitError(`Unable to retrieve secrets from doppler`)
-      error.details = `Please check that your system code is correct`
-      throw error
+
+      // the Doppler CLI doesn't reliably exit with an error code even if it
+      // encountered one so we need to check stderr too
+      if (!errorMsg) {
+        return JSON.parse(secrets)
+      }
+    } catch (err) {
+      if (!errorMsg) {
+        this.logger.warn(`caught error '${err}' when calling Doppler, falling back to Vault`)
+      }
     }
+    if (errorMsg) {
+      this.logger.warn(
+        `doppler CLI failed with the following error logs, falling back to Vault:\n${styles.warningHighlight(
+          errorMsg
+        )}`
+      )
+    }
+
+    // fall back to Vault
+    if (!hasLoggedMigrationWarning) {
+      this.logger.warn(
+        `getting Doppler secrets failed so falling back to the ${styles.warningHighlight(
+          'DEPRECATED'
+        )} Vault secrets manager. please consider migrating to/fixing issues with Doppler.`
+      )
+      hasLoggedMigrationWarning = true
+    }
+    const vault = new Vault.VaultEnvVars(this.logger, {
+      environment: dopplerEnvToVaultMap[this.environment]
+    })
+    return vault.get()
   }
 }
