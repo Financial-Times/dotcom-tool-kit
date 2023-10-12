c83420750f9282b550014ae5c3d2cc5b698fd8ca
Ivo Murrell <ivo.murrell@ft.com>
2023-10-12 16:05:16 +0100

    perf!: add new lazy plugin spec to rest of plugins

--- plugins/serverless/src/index.ts
+++ plugins/serverless/src/index.ts
@@ -3,4 +3,4 @@ import ServerlessProvision from './tasks/provision'
 import ServerlessRun from './tasks/run'
 import ServerlessTeardown from './tasks/teardown'
 
-export const tasks = [ServerlessRun, ServerlessDeploy, ServerlessProvision, ServerlessTeardown]
+export const tasks = { ServerlessRun, ServerlessDeploy, ServerlessProvision, ServerlessTeardown }
