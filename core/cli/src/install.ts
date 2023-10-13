c4265f8b31565b0c72c6139dadb5ea86891194ab
Ivo Murrell <ivo.murrell@ft.com>
2023-10-13 15:07:45 +0100

    feat(core): run hook checks when running tasks if files have changed
    We avoid running checks unconditionally so that we don't have to load
    all plugins that export hooks every time we run a command, but we still
    want to let the user know if their config files have diverged from what
    the hooks have installed. Let's only check the configuration when
    running commands if we see the files have changed, storing hashes in
    .toolkitstate to compare.
    
    For now, the configuration files to check are hardcoded (CircleCI,
    package.json, and .toolkitrc.yml) but in the future we could have hooks
    register which files they modify and check those.

--- core/cli/src/install.ts
+++ core/cli/src/install.ts
@@ -1,9 +1,8 @@
 import { ToolKitError } from '@dotcom-tool-kit/error'
 import { OptionKey, setOptions } from '@dotcom-tool-kit/options'
-import { Hook, HookConstructor } from '@dotcom-tool-kit/types'
 import groupBy from 'lodash/groupBy'
 import type { Logger } from 'winston'
-import { loadConfig, ValidConfig } from './config'
+import { loadConfig, loadHooks, updateHashes, ValidConfig } from './config'
 import { postInstall } from './postInstall'
 
 // implementation of the Array.some method that supports asynchronous predicates
@@ -32,13 +31,7 @@ export default async function installHooks(logger: Logger): Promise<ValidConfig>
   let usesNewCircleCIGroup = false
   // group hooks without an installGroup separately so that their check()
   // method runs independently
-  const hooks: Hook<unknown>[] = await Promise.all(
-    Object.entries(config.hooks).map(async ([hookName, pluginId]) => {
-      const plugin = await import(pluginId)
-      const Hook = plugin.hooks[hookName] as HookConstructor
-      return new Hook(logger, hookName)
-    })
-  )
+  const hooks = await loadHooks(logger, config)
   const groups = groupBy(hooks, (hook) => hook.installGroup ?? '__' + hook.id)
   for (const [groupId, group] of Object.entries(groups)) {
     try {
@@ -80,6 +73,8 @@ export default async function installHooks(logger: Logger): Promise<ValidConfig>
     error.details = errors.map((error) => `- ${error.message}`).join('\n')
     throw error
   }
+
+  await updateHashes()
 
   return config
 }
