ff6913cdb133093bab5ddff5bae6ccf5c851cf7e
Kara Brightwell <kara@153.io>
2023-10-13 15:18:51 +0100

    chore!: remove all current concrete hook subclasses

--- plugins/heroku/src/index.ts
+++ plugins/heroku/src/index.ts
@@ -2,27 +2,5 @@ import HerokuProduction from './tasks/production'
 import HerokuStaging from './tasks/staging'
 import HerokuReview from './tasks/review'
 import HerokuTeardown from './tasks/teardown'
-import { PackageJsonScriptHook } from '@dotcom-tool-kit/package-json-hook'
-
-class BuildRemote extends PackageJsonScriptHook {
-  key = 'heroku-postbuild'
-  hook = 'build:remote'
-}
-
-class CleanupRemote extends PackageJsonScriptHook {
-  key = 'heroku-postbuild'
-  hook = 'cleanup:remote'
-}
-
-class ReleaseRemote extends PackageJsonScriptHook {
-  key = 'heroku-postbuild'
-  hook = 'release:remote'
-}
-
-export const hooks = {
-  'cleanup:remote': CleanupRemote,
-  'release:remote': ReleaseRemote,
-  'build:remote': BuildRemote
-}
 
 export const tasks = { HerokuProduction, HerokuStaging, HerokuReview, HerokuTeardown }
--- plugins/prettier/src/index.ts
+++ plugins/prettier/src/index.ts
@@ -1,15 +1,3 @@
-import { PackageJsonScriptHook } from '@dotcom-tool-kit/package-json-hook'
 import Prettier from './tasks/prettier'
 
 export const tasks = { Prettier }
-
-class FormatLocal extends PackageJsonScriptHook {
-  static description = 'format prettier'
-
-  key = 'format'
-  hook = `format:local`
-}
-
-export const hooks = {
-  'format:local': FormatLocal
-}
