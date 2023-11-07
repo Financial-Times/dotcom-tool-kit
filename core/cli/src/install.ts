b2ebf87af75f346bb732c0c5fad4f1cedafbfc9c
Kara Brightwell <kara@153.io>
2023-11-07 17:09:55 +0000

    feat: reïntroduce validating plugins when we load them

--- core/cli/src/install.ts
+++ core/cli/src/install.ts
@@ -4,6 +4,7 @@ import groupBy from 'lodash/groupBy'
 import type { Logger } from 'winston'
 import { loadConfig, loadHooks, updateHashes, ValidConfig } from './config'
 import { postInstall } from './postInstall'
+import { unwrapValidated } from '@dotcom-tool-kit/types'
 
 // implementation of the Array.every method that supports asynchronous predicates
 async function asyncEvery<T>(arr: T[], pred: (x: T) => Promise<boolean>): Promise<boolean> {
@@ -27,7 +28,8 @@ export default async function installHooks(logger: Logger): Promise<ValidConfig>
   const errors: Error[] = []
   // group hooks without an installGroup separately so that their check()
   // method runs independently
-  const hooks = await loadHooks(logger, config)
+  const hooks = unwrapValidated(await loadHooks(logger, config), 'hooks are invalid')
+
   const groups = groupBy(hooks, (hook) => hook.installGroup ?? '__' + hook.id)
   for (const group of Object.values(groups)) {
     try {
