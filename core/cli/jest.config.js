d92a28a308d2be02f6853a54558c2b6ec03d4aa0
Kara Brightwell <kara@153.io>
2023-11-21 11:28:18 +0000

    test: use test-specific tsconfig files instead of inline ts-jest compiler options

--- core/cli/jest.config.js
+++ core/cli/jest.config.js
@@ -1,13 +1,12 @@
 const base = require('../../jest.config.base')
+const path = require('path')
 
 module.exports = {
   ...base,
   globals: {
     'ts-jest': {
-      tsconfig: {
-        ...base.globals['ts-jest'].tsconfig,
-        resolveJsonModule: true
-      }
+      ...base.globals['ts-jest'],
+      tsconfig: path.resolve(__dirname, './tsconfig.test.json')
     }
   }
 }
