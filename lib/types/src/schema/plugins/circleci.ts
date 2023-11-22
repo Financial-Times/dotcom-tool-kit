9bd246e99cd7eac509f0923cfa1639e8dd5d20db
Ivo Murrell <ivo.murrell@ft.com>
2023-11-22 14:24:00 +0000

    chore(circleci)!: bump default CircleCI node image used to Node 18
    Don't love that this is a breaking change for the types package rather
    than for circleci but types is going to be getting a major version bump
    from other changes in the next branch anyway.

--- lib/types/src/schema/plugins/circleci.ts
+++ lib/types/src/schema/plugins/circleci.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const CircleCISchema = z.object({
-  nodeVersion: z.string().or(z.string().array()).default('16.14-browsers'),
+  nodeVersion: z.string().or(z.string().array()).default('18.18-browsers'),
   cypressImage: z.string().optional()
 })
 export type CircleCIOptions = z.infer<typeof CircleCISchema>
