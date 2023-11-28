ecbaa8a050af21d900a516ce9d73c7850567950d
Kara Brightwell <kara@153.io>
2023-11-28 14:45:46 +0000

    feat: support trailing string in packagejson options

--- lib/types/src/schema/hooks/package-json.ts
+++ lib/types/src/schema/hooks/package-json.ts
@@ -1,6 +1,17 @@
 import { z } from 'zod'
 
-export const PackageJsonSchema = z.record(z.record(z.union([z.array(z.string()), z.string()])))
+const CommandListSchema = z.union([z.array(z.string()), z.string()])
+export const PackageJsonSchema = z.record(
+  z.record(
+    z.union([
+      CommandListSchema,
+      z.object({
+        commands: CommandListSchema,
+        trailingString: z.string()
+      })
+    ])
+  )
+)
 
 export type PackageJsonOptions = z.infer<typeof PackageJsonSchema>
 
