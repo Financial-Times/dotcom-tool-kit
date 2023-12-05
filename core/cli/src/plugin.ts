8377975a054f0b6505ea47877af5e781ad563b7d
Kara Brightwell <kara@153.io>
2023-12-05 17:19:31 +0000

    docs: write a thirty-five line comment for a forty-line function

--- core/cli/src/plugin.ts
+++ core/cli/src/plugin.ts
@@ -281,6 +281,31 @@ export function resolvePlugin(plugin: Plugin, config: ValidPluginsConfig, logger
   config.resolvedPlugins.add(plugin)
 }
 
+// this function recursively collects all the hook installation requests from all plugins,
+// and merges them into a single, flat array of HookInstallation objects and/or Conflicts.
+//
+// it works depth-first (i.e. recurses into child plugins first), and considers how to
+// merge options or create conflicts in two stages: 1) when considering all installations
+// from child plugins, and 2) when considering how a parent plugin would override its
+// children. these steps are separate as a particular parent might not provide an override
+// for all its children, and different hooks could expect different ways of resolving
+// conflicts.
+//
+// the actual logic for this is delegated to static methods on Hook classes,
+// `Hook.mergeChildInstallations` and `Hook.overrideChildInstallations`, so separate hooks
+// can provide different logic for these steps.
+//
+// the default logic in the base Hook class is to always consider multiple installations
+// from child plugins as a conflict, and always consider a installation in a parent as
+// completely replacing any installations from children.
+//
+// for example, for a plugin `p` that depends on children `a`, `b`, and `c` that all provide
+// configuration for the `PackageJson` hook, this function will:
+//   - do all this logic for `a`, `b`, and `c`
+//   - call `Hook.mergeChildInstallations` with the appropriate concrete Hook class, and
+//     the resulting installations and/or conflicts from `a`, `b`, and `c`
+//   - call `Hook.overrideChildInstallations` with the appropriate concrete Hook class, and
+//     the resulting installations and/or conflicts from `Hook.mergeChildInstallations` and `p`
 export async function reducePluginHookInstallations(
   logger: Logger,
   config: ValidConfig,
