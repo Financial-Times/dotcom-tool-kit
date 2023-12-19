743ce6e6c4221136942c15fbebe41f33050b55e8
Kara Brightwell <kara@153.io>
2023-12-19 11:31:31 +0000

    refactor: move importEntryPoint to its own module

---
+++ core/cli/src/plugin/entry-point.ts
@@ -0,0 +1,59 @@
+import { styles as s } from '@dotcom-tool-kit/logger'
+
+import { Base, EntryPoint, Validated, invalid } from '@dotcom-tool-kit/types'
+import resolvePkg from 'resolve-pkg'
+import { isPlainObject } from 'lodash'
+import { indentReasons } from '../messages'
+
+const isPlainObjectGuard = (value: unknown): value is Record<string, unknown> => isPlainObject(value)
+
+// the subclasses of Base have different constructor signatures so we need to omit
+// the constructor from the type bound here so you can actually pass in a subclass
+export async function importEntryPoint<T extends { name: string } & Omit<typeof Base, 'new'>>(
+  type: T,
+  entryPoint: EntryPoint
+): Promise<Validated<T>> {
+  const resolvedPath = resolvePkg(entryPoint.modulePath, { cwd: entryPoint.plugin.root })
+
+  if (!resolvedPath) {
+    return invalid([
+      `could not find entrypoint ${s.filepath(entryPoint.modulePath)} in plugin ${s.plugin(
+        entryPoint.plugin.id
+      )}`
+    ])
+  }
+
+  let pluginModule: unknown
+  try {
+    pluginModule = await import(resolvedPath)
+  } catch (e) {
+    const err = e as Error
+    return invalid([
+      `an error was thrown when loading entrypoint ${s.filepath(entryPoint.modulePath)} in plugin ${s.plugin(
+        entryPoint.plugin.id
+      )}:\n  ${s.code(indentReasons(err.toString()))}`
+    ])
+  }
+
+  if (
+    isPlainObjectGuard(pluginModule) &&
+    'default' in pluginModule &&
+    typeof pluginModule.default === 'function'
+  ) {
+    const name = pluginModule.default.name
+
+    return type
+      .isCompatible<T>(pluginModule.default)
+      .mapError((reasons) => [
+        `the ${type.name.toLowerCase()} ${s.hook(name)} is not a compatible instance of ${s.code(
+          type.name
+        )}:\n  - ${reasons.join('\n  - ')}`
+      ])
+  } else {
+    return invalid([
+      `entrypoint ${s.filepath(entryPoint.modulePath)} in plugin ${s.plugin(
+        entryPoint.plugin.id
+      )} does not have a ${s.code('default')} export`
+    ])
+  }
+}
