686f84cd30b9e8f022fa7d740cfee4c226e37da8
Kara Brightwell <kara@153.io>
2024-04-08 14:54:21 +0100

    feat!: move jest options to task options and allow configuring env

--- lib/schemas/src/tasks.ts
+++ lib/schemas/src/tasks.ts
@@ -1,10 +1,12 @@
 import { type InferSchemaOptions } from './infer'
 import { BabelSchema } from './tasks/babel'
 import { ESLintSchema } from './tasks/eslint'
+import { JestSchema } from './tasks/jest'
 
 export const TaskSchemas = {
   Babel: BabelSchema,
-  Eslint: ESLintSchema
+  Eslint: ESLintSchema,
+  Jest: JestSchema
 }
 
 export type TaskOptions = InferSchemaOptions<typeof TaskSchemas>
--- lib/schemas/src/tasks/jest.ts
+++ lib/schemas/src/tasks/jest.ts
@@ -1,9 +1,10 @@
 import { z } from 'zod'
 
 export const JestSchema = z.object({
-  configPath: z.string().optional()
+  configPath: z.string().optional(),
+  mode: z.union([z.literal('ci'), z.literal('local')])
 })
-export type JestMode = 'ci' | 'local'
+
 export type JestOptions = z.infer<typeof JestSchema>
 
 export const Schema = JestSchema
