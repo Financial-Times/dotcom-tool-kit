3cc635c7afe8a63d8b20c634124e4d46dfa9e4ee
Kara Brightwell <kara@153.io>
2024-04-08 15:42:25 +0100

    feat!: move nodemon options to task options

--- lib/schemas/src/plugins.ts
+++ lib/schemas/src/plugins.ts
@@ -6,7 +6,6 @@ import { HerokuSchema } from './plugins/heroku'
 import { LintStagedNpmSchema } from './plugins/lint-staged-npm'
 import { SmokeTestSchema } from './plugins/n-test'
 import { NextRouterSchema } from './plugins/next-router'
-import { NodemonSchema } from './plugins/nodemon'
 import { Pa11ySchema } from './plugins/pa11y'
 import { PrettierSchema } from './plugins/prettier'
 import { ServerlessSchema } from './plugins/serverless'
@@ -25,7 +24,6 @@ export const PluginSchemas = {
   '@dotcom-tool-kit/lint-staged-npm': LintStagedNpmSchema,
   '@dotcom-tool-kit/n-test': SmokeTestSchema,
   '@dotcom-tool-kit/next-router': NextRouterSchema,
-  '@dotcom-tool-kit/nodemon': NodemonSchema,
   '@dotcom-tool-kit/pa11y': Pa11ySchema,
   '@dotcom-tool-kit/prettier': PrettierSchema,
   '@dotcom-tool-kit/serverless': ServerlessSchema,
--- lib/schemas/src/tasks.ts
+++ lib/schemas/src/tasks.ts
@@ -4,13 +4,15 @@ import { ESLintSchema } from './tasks/eslint'
 import { JestSchema } from './tasks/jest'
 import { MochaSchema } from './tasks/mocha'
 import { NodeSchema } from './tasks/node'
+import { NodemonSchema } from './tasks/nodemon'
 
 export const TaskSchemas = {
   Babel: BabelSchema,
   Eslint: ESLintSchema,
   Jest: JestSchema,
   Mocha: MochaSchema,
-  Node: NodeSchema
+  Node: NodeSchema,
+  Nodemon: NodemonSchema
 }
 
 export type TaskOptions = InferSchemaOptions<typeof TaskSchemas>
