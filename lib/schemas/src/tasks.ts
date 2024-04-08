08b092bc1f3af5ee56413f17a7affdab3eed057e
Kara Brightwell <kara@153.io>
2024-04-08 15:21:39 +0100

    feat!: move mocha options to task options

--- lib/schemas/src/tasks.ts
+++ lib/schemas/src/tasks.ts
@@ -2,11 +2,13 @@ import { type InferSchemaOptions } from './infer'
 import { BabelSchema } from './tasks/babel'
 import { ESLintSchema } from './tasks/eslint'
 import { JestSchema } from './tasks/jest'
+import { MochaSchema } from './tasks/mocha'
 
 export const TaskSchemas = {
   Babel: BabelSchema,
   Eslint: ESLintSchema,
-  Jest: JestSchema
+  Jest: JestSchema,
+  Mocha: MochaSchema
 }
 
 export type TaskOptions = InferSchemaOptions<typeof TaskSchemas>
