e6cb8b1ec053db44831f21124f73d79c969bfd4d
Kara Brightwell <kara@153.io>
2024-04-09 17:24:27 +0100

    test(heroku): update tests for new options

--- plugins/heroku/test/tasks/production.test.ts
+++ plugins/heroku/test/tasks/production.test.ts
@@ -3,7 +3,7 @@ import Production from '../../src/tasks/production'
 import * as utils from '../../src/promoteStagingToProduction'
 import winston, { Logger } from 'winston'
 
-const logger = (winston as unknown) as Logger
+const logger = winston as unknown as Logger
 
 jest.mock('@dotcom-tool-kit/state', () => {
   return {
@@ -15,29 +15,32 @@ jest.mock('../../src/scaleDyno')
 const mockPromoteStagingToProduction = jest.spyOn(utils, 'promoteStagingToProduction')
 jest.spyOn(Production.prototype, 'fetchIfAppHasDeployed').mockImplementation(() => Promise.resolve(true))
 
-const productionOptions = {
+const pluginOptions = {
   pipeline: 'next-health',
-  systemCode: 'next-health',
+  systemCode: 'next-health'
+}
+
+const productionOptions = {
   scaling: { 'test-app': { web: { size: 'standard-1x', quantity: 1 } } }
 }
 
 describe('staging', () => {
   it('should call set slug with slug id and system code', async () => {
     mockPromoteStagingToProduction.mockImplementation(() => Promise.resolve([]))
-    const task = new Production(logger, 'HerokuProduction', productionOptions)
+    const task = new Production(logger, 'HerokuProduction', pluginOptions, productionOptions)
     await task.run()
 
     expect(utils.promoteStagingToProduction).toBeCalledWith(expect.anything(), 'slug-id', 'next-health')
   })
 
   it('should resolve when completed successfully', async () => {
-    const task = new Production(logger, 'HerokuProduction', productionOptions)
+    const task = new Production(logger, 'HerokuProduction', pluginOptions, productionOptions)
     await expect(task.run()).resolves.not.toThrow()
   })
 
   it('should throw if it completes unsuccessfully', async () => {
     mockPromoteStagingToProduction.mockImplementation(() => Promise.reject())
-    const task = new Production(logger, 'HerokuProduction', productionOptions)
+    const task = new Production(logger, 'HerokuProduction', pluginOptions, productionOptions)
     await expect(task.run()).rejects.toThrowError()
   })
 })
