aa01fb6d8000858efd02164f84243f2e2e2d04fb
Kara Brightwell <kara@153.io>
2024-04-12 11:04:22 +0100

    feat: make ports optional in node and nodemon tasks

--- lib/schemas/src/tasks/node.ts
+++ lib/schemas/src/tasks/node.ts
@@ -4,7 +4,7 @@ export const NodeSchema = z.object({
   entry: z.string().default('./server/app.js'),
   args: z.string().array().optional(),
   useDoppler: z.boolean().default(true),
-  ports: z.number().array().default([3001, 3002, 3003])
+  ports: z.union([z.number().array(), z.literal(false)]).default([3001, 3002, 3003])
 })
 export type NodeOptions = z.infer<typeof NodeSchema>
 
