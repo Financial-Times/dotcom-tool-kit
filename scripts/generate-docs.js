d19bfe77d38674671eccd4995b9fa0e90fe0b541
Kara Brightwell <kara@153.io>
2024-04-23 13:00:41 +0100

    build: load a plugin's toolkitrc for list of hooks & tasks

--- scripts/generate-docs.js
+++ scripts/generate-docs.js
@@ -1,13 +1,64 @@
+const { loadToolKitRC } = require('../core/cli/lib/rc-file')
 const { TaskSchemas } = require('../lib/schemas/lib/tasks')
 const { HookSchemas } = require('../lib/schemas/lib/hooks')
 const { PluginSchemas } = require('../lib/schemas/lib/plugins')
+const { default: $t } = require('endent')
+const logger = require('winston')
 
 const { convertSchemas, formatModelsAsMarkdown } = require('zod2md')
+const path = require('path')
 
-const converted = convertSchemas([{ name: 'Babel', schema: TaskSchemas.Babel }])
+function formatSchemas(title, schemas) {
+  const converted = convertSchemas(schemas)
+  return formatModelsAsMarkdown(converted, { title })
+}
 
-console.log(
-  formatModelsAsMarkdown(converted, {
-    title: 'Options'
-  })
-)
+function formatPluginSchemas({ plugin, hooks, tasks }) {
+  const tasksWithSchemas = tasks.filter((task) => TaskSchemas[task])
+  const hooksWithSchemas = hooks.filter((hook) => HookSchemas[hook])
+  return $t`
+    ${
+      PluginSchemas[plugin]
+        ? formatSchemas(plugin, [
+            {
+              name: ' ',
+              schema: PluginSchemas[plugin]
+            }
+          ])
+        : ''
+    }
+    ${
+      tasksWithSchemas.length
+        ? formatSchemas(
+            'Tasks',
+            tasksWithSchemas.map((task) => ({
+              name: task,
+              schema: TaskSchemas[task]
+            }))
+          )
+        : ''
+    }
+    ${
+      hooksWithSchemas.length
+        ? formatSchemas(
+            'Hooks',
+            hooksWithSchemas.map((hook) => ({
+              name: hook,
+              schema: HookSchemas[hook]
+            }))
+          )
+        : ''
+    }
+	`
+}
+
+async function getPluginSchemas(plugin) {
+  const rcFile = await loadToolKitRC(logger, path.join('plugins', plugin), false)
+  return {
+    plugin: `@dotcom-tool-kit/${plugin}`,
+    hooks: Object.keys(rcFile.installs),
+    tasks: Object.keys(rcFile.tasks)
+  }
+}
+
+getPluginSchemas('heroku').then(formatPluginSchemas).then(console.log, console.error)
