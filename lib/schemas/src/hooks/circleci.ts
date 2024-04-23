77731ac525368e07155146ca40ffad4a8dba61c3
Kara Brightwell <kara@153.io>
2024-04-23 16:39:46 +0100

    docs(circleci): add descriptions for circleci options

--- lib/schemas/src/hooks/circleci.ts
+++ lib/schemas/src/hooks/circleci.ts
@@ -32,12 +32,37 @@ export const CircleCiCustomConfig = z.record(z.unknown())
 export type CircleCiCustomConfig = z.infer<typeof CircleCiCustomConfig>
 
 export const CircleCiSchema = z.object({
-  executors: z.array(CircleCiExecutor).optional(),
-  jobs: z.array(CircleCiJob).optional(),
-  workflows: z.array(CircleCiWorkflow).optional(),
-  custom: CircleCiCustomConfig.optional(),
-  disableBaseConfig: z.boolean().optional()
+  executors: z
+    .array(CircleCiExecutor)
+    .optional()
+    .describe('an array of additional CircleCI executors to output in the generated config.'),
+  jobs: z
+    .array(CircleCiJob)
+    .optional()
+    .describe(
+      'an array of additional CircleCI jobs to output in the generated config. these are used for running Tool Kit commands. for running arbitrary shell commands, use `custom`.'
+    ),
+  workflows: z
+    .array(CircleCiWorkflow)
+    .optional()
+    .describe(
+      'an array of additional CircleCI workflows to output in the generated config. these reference jobs defined in the `jobs` option.'
+    ),
+  custom: CircleCiCustomConfig.optional().describe(
+    'arbitrary additional CircleCI configuration that will be merged into the Tool Kit-generated config.'
+  ),
+  disableBaseConfig: z
+    .boolean()
+    .optional()
+    .describe(
+      'set to `true` to omit the Tool Kit CircleCI boilerplate. should be used along with `custom` to provide your own boilerplate.'
+    )
 })
+  .describe(`This hook automatically manages \`.circleci/config.yml\` in your repo to provide configuration for CircleCI workflows to run Tool Kit commands and tasks.
+
+Options provided in your repository's \`.toolkitrc.yml\` for this hook are merged with any Tool Kit plugin that also provides options for the hook.
+
+Unless they conflict, your options are appended to options from plugins, allowing you to define custom CircleCI jobs and workflows in your repository that work alongside those from plugins.`)
 
 export type CircleCiOptions = z.infer<typeof CircleCiSchema>
 
--- lib/schemas/src/plugins/circleci.ts
+++ lib/schemas/src/plugins/circleci.ts
@@ -1,7 +1,13 @@
 import { z } from 'zod'
 
 export const CircleCISchema = z.object({
-  cimgNodeVersions: z.string().array().default(['18.19-browsers'])
+  cimgNodeVersions: z
+    .string()
+    .array()
+    .default(['18.19-browsers'])
+    .describe(
+      'list of CircleCI [Node.js image versions](https://circleci.com/developer/images/image/cimg/node) to use. if more than one is provided, a [matrix build](https://circleci.com/docs/using-matrix-jobs/) will be generated in your CircleCI config.'
+    )
 })
 export type CircleCIOptions = z.infer<typeof CircleCISchema>
 
