04bf07804249cc60ae19d6cd222ba02aa8463fa6
Kara Brightwell <kara@153.io>
2024-04-23 17:25:22 +0100

    docs(pa11y): move docs to schema descriptinos

--- lib/schemas/src/tasks/pa11y.ts
+++ lib/schemas/src/tasks/pa11y.ts
@@ -1,8 +1,10 @@
 import { z } from 'zod'
 
-export const Pa11ySchema = z.object({
-  configFile: z.string().optional()
-})
+export const Pa11ySchema = z
+  .object({
+    configFile: z.string().optional().describe('Path to the config file')
+  })
+  .describe('runs `pa11y-ci` to execute Pa11y tests')
 
 export type Pa11yOptions = z.infer<typeof Pa11ySchema>
 
