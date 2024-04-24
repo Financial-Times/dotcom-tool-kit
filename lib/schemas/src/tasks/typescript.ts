60d8b74c9244716a01fc632124af9e107c8af42a
Kara Brightwell <kara@153.io>
2024-04-24 09:07:08 +0100

    docs(typescript): autogenerate options docs

--- lib/schemas/src/tasks/typescript.ts
+++ lib/schemas/src/tasks/typescript.ts
@@ -1,11 +1,28 @@
 import { z } from 'zod'
 
-export const TypeScriptSchema = z.object({
-  configPath: z.string().optional(),
-  build: z.literal(true).optional(),
-  watch: z.literal(true).optional(),
-  noEmit: z.literal(true).optional()
-})
+export const TypeScriptSchema = z
+  .object({
+    configPath: z
+      .string()
+      .optional()
+      .describe(
+        "to the [TypeScript config file](https://www.typescriptlang.org/tsconfig). Uses TypeScript's own [tsconfig.json resolution](https://www.typescriptlang.org/docs/handbook/tsconfig-json.html#using-tsconfigjson-or-jsconfigjson) by default"
+      ),
+    build: z
+      .literal(true)
+      .optional()
+      .describe(
+        'Run Typescript in [build mode](https://www.typescriptlang.org/docs/handbook/project-references.html#build-mode-for-typescript).'
+      ),
+    watch: z.literal(true).optional().describe('Run Typescript in watch mode.'),
+    noEmit: z
+      .literal(true)
+      .optional()
+      .describe(
+        'Run Typescript with `--noEmit`, for checking your types without outputting compiled Javascript.'
+      )
+  })
+  .describe('Compile code with `tsc`.')
 
 export type TypeScriptOptions = z.infer<typeof TypeScriptSchema>
 
