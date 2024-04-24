6293bc813701b023cf7efd2d542f69c71a1500a4
Kara Brightwell <kara@153.io>
2024-04-24 17:00:36 +0100

    feat(monorepo): run workspace commands in parallel

--- plugins/monorepo/src/tasks/workspace-command.ts
+++ plugins/monorepo/src/tasks/workspace-command.ts
@@ -3,23 +3,37 @@ import mapWorkspaces from '@npmcli/map-workspaces'
 import fs from 'fs/promises'
 import path from 'path'
 import { loadConfig } from 'dotcom-tool-kit/lib/config'
+import { runTasksFromConfig } from 'dotcom-tool-kit/lib/tasks'
+import { ToolKitError } from '@dotcom-tool-kit/error'
 
 export default class WorkspaceCommand extends Task {
-  async run({ cwd }: TaskRunContext) {
+  async runPackageCommand(packageId: string, packagePath: string, command: string, files?: string[]) {
+    const config = await loadConfig(this.logger, { root: packagePath })
+
+    return runTasksFromConfig(this.logger.child({ packageId }), config, [command], files)
+  }
+
+  async run({ cwd, command, files }: TaskRunContext) {
     const pkg = JSON.parse(await fs.readFile(path.join(cwd, 'package.json'), 'utf8'))
 
     const workspaces = await mapWorkspaces({ cwd, pkg })
 
-    const configs = Object.fromEntries(
-      await Promise.all(
-        Array.from(workspaces, async ([id, packagePath]) => [
-          id,
-          await loadConfig(this.logger, { root: packagePath })
-        ])
-      )
+    const packagePromises: Array<Promise<void>> = []
+
+    for (const [id, packagePath] of workspaces) {
+      packagePromises.push(this.runPackageCommand(id, packagePath, command, files))
+    }
+
+    const results = await Promise.allSettled(packagePromises)
+    const erroredCommands = results.filter(
+      (result): result is PromiseRejectedResult => result.status === 'rejected'
     )
 
-    // eslint-disable-next-line no-console
-    console.log(configs)
+    if (erroredCommands.length) {
+      // TODO improve error messages
+      const error = new ToolKitError(`error running workspace command ${command}`)
+      error.details = erroredCommands.map((result) => result.reason.toString()).join('\n\n')
+      throw error
+    }
   }
 }
