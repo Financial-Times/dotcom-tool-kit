91afc2ec580ec646858c55e5a38401ceceabc2c9
Kara Brightwell <kara@153.io>
2024-04-25 12:14:53 +0100

    build: better error messaging and stash handling for docs script

--- scripts/generate-and-commit-docs.sh
+++ scripts/generate-and-commit-docs.sh
@@ -2,16 +2,32 @@
 
 set -euo pipefail
 
-git stash --staged --quiet
+if git diff --quiet --exit-code && git diff --cached --quiet --exit-code; then
+	HAS_CHANGES="no"
+else
+	HAS_CHANGES="yes"
+fi
+
+if [ "$HAS_CHANGES" == "yes" ]; then
+	git stash --staged --quiet
+fi
 
 node ./scripts/generate-docs.js
 
 if ! git diff --quiet --exit-code; then
 	git commit -m 'docs: automatically regenerate schema docs' plugins/\*/readme.md
 
-	echo '!! automatically generated documentation has been regenerated and committed. please push again'
-	git stash pop
+	echo ''
+	echo -e "\e[31m!! automatically generated documentation has been regenerated and committed. please push again\e[0m"
+	echo ''
+
+	if [ "$HAS_CHANGES" == "yes" ]; then
+		git stash pop
+	fi
+
 	exit 1
 fi
 
-git stash pop
+if [ "$HAS_CHANGES" == "yes" ]; then
+	git stash pop
+fi
