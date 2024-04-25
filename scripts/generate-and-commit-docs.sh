84f7d0ba74204436e2d2f64ad4951c1117b6765c
Kara Brightwell <kara@153.io>
2024-04-25 12:06:46 +0100

    build: pass the glob for readmes through to git

--- scripts/generate-and-commit-docs.sh
+++ scripts/generate-and-commit-docs.sh
@@ -7,7 +7,7 @@ git stash --staged --quiet
 node ./scripts/generate-docs.js
 
 if ! git diff --quiet --exit-code; then
-	git commit -m 'docs: automatically regenerate schema docs' plugins/*/readme.md
+	git commit -m 'docs: automatically regenerate schema docs' plugins/\*/readme.md
 
 	echo '!! automatically generated documentation has been regenerated and committed. please push again'
 	git stash pop
