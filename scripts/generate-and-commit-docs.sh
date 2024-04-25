339636a51fc6a20d9ffbd76549e5c7d8c75748ef
Kara Brightwell <kara@153.io>
2024-04-25 11:40:11 +0100

    build: add pre-push hook to regenerate docs

---
+++ scripts/generate-and-commit-docs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+git stash --staged --quiet
+
+node ./scripts/generate-docs.js
+
+if ! git diff --quiet --exit-code; then
+	git commit -m 'docs: automatically regenerate schema docs' plugins/*/readme.md
+
+	echo '!! automatically generated documentation has been regenerated and committed. please push again'
+	git stash pop
+	exit 1
+fi
+
+git stash pop
