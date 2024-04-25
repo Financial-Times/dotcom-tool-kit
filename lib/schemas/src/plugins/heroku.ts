b21c266800a18eeda3406bab7ad2feac22b687c9
Kara Brightwell <kara@153.io>
2024-04-25 10:40:11 +0100

    docs(heroku): autogenerate options docs

--- lib/schemas/src/plugins/heroku.ts
+++ lib/schemas/src/plugins/heroku.ts
@@ -2,8 +2,16 @@ import { PromptGenerators } from '../prompts'
 import { z } from 'zod'
 
 export const HerokuSchema = z.object({
-  pipeline: z.string().describe('this can be found at https://dashboard.heroku.com/pipelines/[APP_ID]'),
-  systemCode: z.string().describe('this can be found at https://biz-ops.in.ft.com/System/[APP_NAME]')
+  pipeline: z
+    .string()
+    .describe(
+      "the ID of your app's Heroku pipeline. this can be found at https://dashboard.heroku.com/pipelines/[PIPELINE_ID]"
+    ),
+  systemCode: z
+    .string()
+    .describe(
+      "your app's Biz Ops system code. this can be found at https://biz-ops.in.ft.com/System/[SYSTEM_CODE]"
+    )
 })
 
 export type HerokuOptions = z.infer<typeof HerokuSchema>
