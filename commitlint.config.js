254982beac85f6a6fa0fd355de78af27175f8b0c
Kara Brightwell <kara@153.io>
2024-05-29 10:20:13 +0100

    backstage: disallow chore commits, introduce backstage commits
    CPP-2112

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['backstage', 'build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
+    ]
+  }
+}
