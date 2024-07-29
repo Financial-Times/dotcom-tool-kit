f80f457d9b37eb06741d72a2ff1740388bdf4404
Kara Brightwell <kara@153.io>
2024-07-29 17:10:19 +0100

    refactor: add plugin property to task class

--- lib/base/src/task.ts
+++ lib/base/src/task.ts
@@ -3,6 +3,7 @@ import { Base } from './base'
 import { taskSymbol, typeSymbol } from './symbols'
 import type { Logger } from 'winston'
 import type { ValidConfig } from '@dotcom-tool-kit/config'
+import { Plugin } from '@dotcom-tool-kit/plugin'
 import type { Default } from './type-utils'
 import type { ReadonlyDeep } from 'type-fest'
 
@@ -33,7 +34,8 @@ export abstract class Task<
     logger: Logger,
     public id: string,
     public pluginOptions: z.output<Default<Options['plugin'], z.ZodObject<Record<string, never>>>>,
-    public options: z.output<Default<Options['task'], z.ZodObject<Record<string, never>>>>
+    public options: z.output<Default<Options['task'], z.ZodObject<Record<string, never>>>>,
+    public plugin: Plugin
   ) {
     super()
     this.logger = logger.child({ task: id })
@@ -47,7 +49,8 @@ export type TaskConstructor = {
     logger: Logger,
     id: string,
     pluginOptions: Partial<z.infer<O['plugin']>>,
-    options: Partial<z.infer<O['task']>>
+    options: Partial<z.infer<O['task']>>,
+    plugin: Plugin
   ): Task<O>
 }
 
