91beb375dcc89b3d85b66d74764df25ac376d773
Kara Brightwell <kara@153.io>
2024-11-04 11:36:01 +0000

    docs(monorepo): add readme

--- plugins/monorepo/src/tasks/workspace-command.ts
+++ plugins/monorepo/src/tasks/workspace-command.ts
@@ -8,8 +8,67 @@ import { runTasksFromConfig } from 'dotcom-tool-kit/lib/tasks'
 import { ToolKitError } from '@dotcom-tool-kit/error'
 
 const WorkspaceCommandSchema = z.object({
-  command: z.string().optional()
+  command: z
+    .string()
+    .optional()
+    .describe('A specific command to run instead of the command that ran this task.')
 })
+  .describe(`Runs a Tool Kit command in all workspace packages that have that command. By default, runs the command that was used to run this task.
+
+For example, imagine a monorepo with these \`.toolkitrc.yml\` files:
+
+<details><summary><code>.toolkitrc.yml</code></summary>
+
+~~~yml
+commands:
+  run:local: WorkspaceCommand
+  build:local: WorkspaceCommand
+~~~
+
+</details>
+
+<details><summary><code>packages/api/.toolkitrc.yml</code></summary>
+
+~~~yml
+commands:
+  run:local: Node
+~~~
+
+</details>
+
+<details><summary><code>packages/client/.toolkitrc.yml</code></summary>
+
+~~~yml
+commands:
+  build:local: TypeScript
+~~~
+
+</details>
+
+<details><summary><code>packages/components/.toolkitrc.yml</code></summary>
+
+~~~yml
+commands:
+  build:local: Webpack
+  run:local:
+    Webpack:
+      watch: true
+~~~
+
+</details>
+
+Running \`dotcom-tool-kit run:local\` at the root level will run the \`Node\` task in \`packages/api\` and the \`Webpack\` task in watch mode in \`packages/components\`; running \`dotcom-tool-kit build:local\` will run \`TypeScript\` in \`packages/client\` and \`Webpack\` in \`packages/components\`.
+
+To run a particular command in the workspace instead of dynamically inferring the command from which was run at root level, set the \`command\` option for the task:
+
+~~~yml
+commands:
+  build:ci:
+    WorkspaceCommand:
+      command: build:local
+~~~
+
+`)
 export { WorkspaceCommandSchema as schema }
 
 export default class WorkspaceCommand extends Task<{ task: typeof WorkspaceCommandSchema }> {
