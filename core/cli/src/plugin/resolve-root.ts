1b5d8b9d6fb8655043bb9ee5bc30b6311373c439
Kara Brightwell <kara@153.io>
2024-11-26 15:29:37 +0000

    test: mock require.resolve to use node's implementation so the tests demonstrate the issue

--- core/cli/src/plugin/resolve-root.ts
+++ core/cli/src/plugin/resolve-root.ts
@@ -1,10 +1,12 @@
 import path from "path"
+import { resolve } from "./require-resolve"
 
 export function resolveRoot(id: string, root: string): string {
   // resolve the .toolkitrc.yml of a plugin as many plugins don't have valid
   // entrypoints now that we're intending their tasks/hooks to be loaded via
   // entrypoints defined in config
   const pluginPath = path.join(id, '.toolkitrc.yml')
-  const resolvedPath = require.resolve(pluginPath, { paths: [root] })
+  const resolvedPath = resolve(pluginPath, { paths: [root] })
+
   return path.dirname(resolvedPath)
 }
--- core/cli/test/resolve-root.test.ts
+++ core/cli/test/resolve-root.test.ts
@@ -1,6 +1,35 @@
 import path from 'path'
 import { resolveRoot } from '../src/plugin/resolve-root'
 
+// HACK:KB:20241126 Jest's require.resolve implementation works differently from Node's
+// when provided the `paths` option, in a way that let our tests pass but didn't work in
+// Node. there's no way to use Node's actual implementation of require.resolve in Jest,
+// so we need to call Node as a subprocess and have it run require.resolve for us.
+// https://github.com/jestjs/jest/issues/9502
+jest.mock('../src/plugin/require-resolve', () => {
+  const { spawnSync } = jest.requireActual('child_process')
+
+  return {
+    resolve: (id: string, options?: { paths?: string[] }) => {
+      const result = spawnSync(
+        'node',
+        ['-e', `process.stdout.write(require.resolve(${JSON.stringify(id)}${options ? ', ' + JSON.stringify(options) : ''}))`],
+        {cwd: __dirname}
+      )
+
+      if(result.error) {
+        throw result.error
+      }
+
+      if(result.status) {
+        throw new Error(result.stderr.toString())
+      }
+
+      return result.stdout.toString('utf8')
+    }
+  }
+})
+
 describe('plugin loading', () => {
   describe('resolveRoot', () => {
     it('should resolve a plugin specified as a node_module', () => {
