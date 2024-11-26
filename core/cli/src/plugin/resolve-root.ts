887663109ac9b0cfb4c39fb038ac111806d0208d
Kara Brightwell <kara@153.io>
2024-11-26 15:30:58 +0000

    fix: preserve relative paths in resolveRoot to fix custom plugin loading

--- core/cli/src/plugin/resolve-root.ts
+++ core/cli/src/plugin/resolve-root.ts
@@ -2,11 +2,12 @@ import path from "path"
 import { resolve } from "./require-resolve"
 
 export function resolveRoot(id: string, root: string): string {
+  const isRelative = id.startsWith('./')
   // resolve the .toolkitrc.yml of a plugin as many plugins don't have valid
   // entrypoints now that we're intending their tasks/hooks to be loaded via
   // entrypoints defined in config
   const pluginPath = path.join(id, '.toolkitrc.yml')
-  const resolvedPath = resolve(pluginPath, { paths: [root] })
+  const resolvedPath = resolve(isRelative ? './' + pluginPath :  pluginPath, { paths: [root] })
 
   return path.dirname(resolvedPath)
 }
