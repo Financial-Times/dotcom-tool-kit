fa2346df559e905cd31c2aed1d32692ba5a2908a
Ivo Murrell <ivo.murrell@ft.com>
2024-12-05 13:57:29 +0000

    feat(cloudsmith): hardcode Cloudsmith organisation
    We'll likely never the flexibility to set a different organisation.
    
    Co-Authored-By: Rowan Manning <rowanmanning@users.noreply.github.com>

--- lib/schemas/src/plugins/cloudsmith.ts
+++ lib/schemas/src/plugins/cloudsmith.ts
@@ -1,6 +1,5 @@
 import { z } from 'zod'
 
 export const CloudsmithSchema = z.object({
-  organisation: z.string().optional().describe('the Cloudsmith organisation'),
   serviceAccount: z.string().optional().describe('the Cloudsmith service account')
 })
