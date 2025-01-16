79f0de8c752dbe474dc4c12cbae7a94b2f588f3c
Ivo Murrell <ivo.murrell@ft.com>
2025-01-16 12:39:29 +0000

    backstage(schemas): delete obsolete package
    Now that schemas are colocated with plugins, the schema package is no
    longer used. Let's delete it so that release-please doesn't
    inadvertently bump new releases, and to ensure that people don't
    accidentally add schema changes here rather than within plugins.
    
    `core` will still depend on it for now for backwards compatibility, but
    it will be able to from the latest version in the registry rather than
    from the workspace.

--- lib/schemas/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './hooks'
-export * from './plugins'
-export * from './prompts'
-export * from './tasks'
--- lib/schemas/src/plugins/dotcom-tool-kit.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-export const RootSchema = z.object({
-  allowNativeFetch: z.boolean().default(false)
-})
-export type RootOptions = z.infer<typeof RootSchema>
-
-export const Schema = RootSchema
