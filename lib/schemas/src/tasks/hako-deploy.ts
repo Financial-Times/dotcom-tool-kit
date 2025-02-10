27b6495c1e8ad244230155d97f6f421b0b3bf9bd
CyntiBinti <cynthia.mbulu@ft.com>
2025-02-10 13:21:13 +0000

    feat: update schema for hakoDeploy based on PR feedback
    Added HakoEnvironmentNames as part of the zod schema to support clearer
    validation
    
    Co-Authored-By: Rowan Manning <rowanmanning@users.noreply.github.com>
    Co-Authored-By: Alex Muller <alexmuller@users.noreply.github.com>

--- lib/schemas/src/tasks/hako-deploy.ts
+++ lib/schemas/src/tasks/hako-deploy.ts
@@ -1,8 +1,12 @@
 import { z } from 'zod'
 
+const HakoEnvironmentNames = z.enum(['ft-com-prod-eu', 'ft-com-prod-us', 'ft-com-test-eu'])
+
+export type HakoEnvironmentNames = (typeof HakoEnvironmentNames.options)[number]
+
 export const HakoDeploySchema = z
   .object({
-    environments: z.array(z.string()).describe('the Hako environments to deploy an image to')
+    environments: z.array(HakoEnvironmentNames).describe('the Hako environments to deploy an image to')
   })
   .describe('Deploy to ECS via the Hako CLI')
 
