6bdfcb93517f927184f41e27a16c015ceb638a5f
CyntiBinti <cynthia.mbulu@ft.com>
2025-02-10 13:23:39 +0000

    feat: update hako deploy plugin with latest hako v0.1.8-alpha
    This updates the hako image name to the latest hako version and also
    reflects recent feedback from PR review
    
    Co-Authored-By: Rowan Manning <rowanmanning@users.noreply.github.com>
    Co-Authored-By: Alex Muller <alexmuller@users.noreply.github.com>

--- plugins/hako/src/tasks/deploy.ts
+++ plugins/hako/src/tasks/deploy.ts
@@ -1,12 +1,11 @@
 import { spawn } from 'child_process'
-import { hookFork, waitOnExit } from '@dotcom-tool-kit/logger'
+import { hookFork, waitOnExit , styles } from '@dotcom-tool-kit/logger'
 import { readState } from '@dotcom-tool-kit/state'
 import { Task } from '@dotcom-tool-kit/base'
 import { ToolKitError } from '@dotcom-tool-kit/error'
-import { HakoDeploySchema } from '@dotcom-tool-kit/schemas/lib/tasks/hako-deploy'
-import { stderr } from 'process'
+import { HakoDeploySchema, HakoEnvironmentNames } from '@dotcom-tool-kit/schemas/lib/tasks/hako-deploy'
 
-const hakoEnvironments: Record<string, string | undefined> = {
+const hakoEnvironments: Record<string, string> = {
   'ft-com-prod-eu': 'eu-west-1',
   'ft-com-prod-us': 'us-east-1',
   'ft-com-test-eu': 'eu-west-1'
@@ -23,24 +22,18 @@ export default class HakoDeploy extends Task<{ task: typeof HakoDeploySchema }> 
 
       if (!awsCredentials.accessKeyId || !awsCredentials.secretAccessKey || !awsCredentials.sessionToken) {
         throw new Error(
-          'no AWS credentials found, use "@dotcom-tool-kit/aws" and AwsAssumeRole before deploying with hako'
+          `no AWS credentials found, use ${styles.plugin('@dotcom-tool-kit/aws')} and ${styles.task(
+            'AwsAssumeRole'
+          )} before deploying with hako`
         )
       }
 
       if (!pushedImages.length) {
         throw new Error(
-          'no Docker images have been built and pushed, use "@dotcom-tool-kit/docker" before deploying with hako'
+          `no Docker images have been built and pushed, use ${styles.plugin(
+            '@dotcom-tool-kit/docker'
+          )} before deploying with hako`
         )
-      }
-
-      for (const environment of deployEnvironments) {
-        if (!hakoEnvironments[environment]) {
-          throw new Error(
-            `hako environment "${environment}" does not exist, use one of ${Object.keys(
-              hakoEnvironments
-            ).join(', ')}`
-          )
-        }
       }
 
       this.logger.info('Pulling hako-cli image')
@@ -49,7 +42,7 @@ export default class HakoDeploy extends Task<{ task: typeof HakoDeploySchema }> 
         'pull',
         '--platform',
         'linux/amd64',
-        'docker.packages.ft.com/financial-times-internal-releases/hako-cli:0.1.7-alpha'
+        'docker.packages.ft.com/financial-times-internal-releases/hako-cli:0.1.8-alpha'
       ])
 
       hookFork(this.logger, 'hako-pull', child)
@@ -57,11 +50,12 @@ export default class HakoDeploy extends Task<{ task: typeof HakoDeploySchema }> 
 
       // store the async promises and promise.all() once for-loop completes
       const deploys: Promise<void>[] = []
+      const hakoImageName = 'docker.packages.ft.com/financial-times-internal-releases/hako-cli:0.1.8-alpha'
 
       for (const { name, tag } of pushedImages) {
         for (const environment of deployEnvironments) {
           this.logger.info(`Deploying image "${name}" with tag "${tag}" to environment "${environment}"`)
-          const awsRegion = hakoEnvironments[environment]
+          const awsRegion = hakoEnvironments[environment as HakoEnvironmentNames]
           const child = spawn('docker', [
             'run',
             '--env',
@@ -74,22 +68,20 @@ export default class HakoDeploy extends Task<{ task: typeof HakoDeploySchema }> 
             `AWS_SESSION_TOKEN=${awsCredentials.sessionToken}`,
             '--platform',
             'linux/amd64',
-            'docker.packages.ft.com/financial-times-internal-releases/hako-cli:0.1.7-alpha',
+            hakoImageName,
             'image',
             'deploy',
             '--image-name',
             name,
             '--image-tag',
             tag,
-            '--image-port',
-            '3001',
             '--app',
             name, // NOTE: the app name MUST match the image name (for now)
             '--env',
             environment
           ])
-          hookFork(this.logger, 'hako run', child)
-          deploys.push(waitOnExit('hako run', child))
+          hookFork(this.logger, 'hako-run', child)
+          deploys.push(waitOnExit('hako-run', child))
         }
       }
 
