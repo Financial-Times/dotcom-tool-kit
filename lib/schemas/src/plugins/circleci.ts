92064a1b913e8b5d7e27d4761fdd29926301069b
Kara Brightwell <kara@153.io>
2025-02-10 16:05:40 +0000

    feat: throw a helpful validation error on legacy nodeVersion option
    have seen this catch people out a few times in the wild, eg:
    
    - https://github.com/Financial-Times/next-stream-page/pull/3289/files#r1935455217
    - https://financialtimes.slack.com/archives/C02TRE2V2Q1/p1739186911452789

--- lib/schemas/src/plugins/circleci.ts
+++ lib/schemas/src/plugins/circleci.ts
@@ -1,26 +1,32 @@
 import { z } from 'zod'
+import { styles as s } from '@dotcom-tool-kit/logger'
 
-export const CircleCISchema = z.object({
-  cimgNodeVersions: z
-    .string()
-    .array()
-    .default(['18.19-browsers'])
-    .describe(
-      'list of CircleCI [Node.js image versions](https://circleci.com/developer/images/image/cimg/node) to use. if more than one is provided, a [matrix build](https://circleci.com/docs/using-matrix-jobs/) will be generated in your CircleCI config.'
-    ),
-  cypressImage: z
-    .string()
-    .optional()
-    .describe(
-      "the Cypress docker image to use. see https://github.com/cypress-io/cypress-docker-images for available images and tags. if this option is present, and you're using the [`circleci-deploy`](../circleci-deploy) plugin, this will override the default `node` executor for the `e2e-test-review` and `e2e-test-staging` jobs."
-    ),
-  tagFilterRegex: z
-    .string()
-    .default(/^v\d+\.\d+\.\d+(-.+)?/.toString())
-    .describe(
-      'the regular expression used to match tags for jobs that should run on tag workflows. by default, matches tags that look like `v1.2.3`; if your releases use a different tag format, change this option to match your tags.'
-    )
-})
+export const CircleCISchema = z
+  .object({
+    cimgNodeVersions: z
+      .string()
+      .array()
+      .default(['18.19-browsers'])
+      .describe(
+        'list of CircleCI [Node.js image versions](https://circleci.com/developer/images/image/cimg/node) to use. if more than one is provided, a [matrix build](https://circleci.com/docs/using-matrix-jobs/) will be generated in your CircleCI config.'
+      ),
+    cypressImage: z
+      .string()
+      .optional()
+      .describe(
+        "the Cypress docker image to use. see https://github.com/cypress-io/cypress-docker-images for available images and tags. if this option is present, and you're using the [`circleci-deploy`](../circleci-deploy) plugin, this will override the default `node` executor for the `e2e-test-review` and `e2e-test-staging` jobs."
+      ),
+    tagFilterRegex: z
+      .string()
+      .default(/^v\d+\.\d+\.\d+(-.+)?/.toString())
+      .describe(
+        'the regular expression used to match tags for jobs that should run on tag workflows. by default, matches tags that look like `v1.2.3`; if your releases use a different tag format, change this option to match your tags.'
+      )
+  })
+  .refine((options) => !('nodeVersion' in options), {
+    message: `the option ${s.code('nodeVersion')} has been replaced by ${s.code('cimgNodeVersions')}`
+  })
+
 export type CircleCIOptions = z.infer<typeof CircleCISchema>
 
 export const Schema = CircleCISchema
