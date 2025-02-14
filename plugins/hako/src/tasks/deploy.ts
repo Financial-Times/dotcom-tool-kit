85510f583f1cd6b4c80908c3f26b5bb249384249
Rowan Manning <accounts+github@rowanmanning.com>
2025-02-14 14:32:24 +0000

    fix: use nullish coalescing operators
    Co-authored-by: Ivo Murrell <ivo.murrell@ft.com>

--- plugins/hako/src/tasks/deploy.ts
+++ plugins/hako/src/tasks/deploy.ts
@@ -15,8 +15,8 @@ export default class HakoDeploy extends Task<{ task: typeof HakoDeploySchema }> 
   async run() {
     this.logger.info('Deploying to Hako')
     try {
-      const awsCredentials = readState('ci')?.awsCredentials || {}
-      const pushedImages = readState('docker')?.pushedImages || []
+      const awsCredentials = readState('ci')?.awsCredentials ?? {}
+      const pushedImages = readState('docker')?.pushedImages ?? []
       const deployEnvironments = this.options.environments
 
       if (!awsCredentials.accessKeyId || !awsCredentials.secretAccessKey || !awsCredentials.sessionToken) {
