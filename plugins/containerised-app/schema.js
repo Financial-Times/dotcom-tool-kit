d4630fc62cc8e51a10e6e79ae53e2e6b16d765a0
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2025-02-14 16:08:04 +0000

    feat: add wrapper plugins for Docker > Hako
    Release-As: 0.1.0
    See-Also: https://financialtimes.atlassian.net/browse/CPREL-1275
    Co-Authored-By: Cynthia Mbulu <CyntiBinti@users.noreply.github.com>

---
+++ plugins/containerised-app/schema.js
@@ -0,0 +1,18 @@
+const z = require('zod')
+
+module.exports = z.object({
+  awsRoleArnStaging: z
+    .string()
+    .regex(/^arn:aws:iam::\d+:role\//, 'Role ARN must be a full IAM role ARN including account number')
+    .describe('the ARN of an IAM role to assume when deploying to staging'),
+  awsRoleArnProduction: z
+    .string()
+    .regex(/^arn:aws:iam::\d+:role\//, 'Role ARN must be a full IAM role ARN including account number')
+    .describe('the ARN of an IAM role to assume when deploying to production'),
+  // HACK: must be either `true` or `undefined` because of the way !toolkit/if-defined works
+  multiregion: z
+    .literal(true)
+    .or(z.undefined())
+    .default(undefined)
+    .describe('Whether the app is deployed across both EU and US regions')
+})
