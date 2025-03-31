7d5b0b190d5d9edc95b9eba68a6f0b13a739d63f
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2025-03-31 15:22:51 +0100

    feat: add a node-test plugin
    This adds a node-test plugin using `node:test#run()`. This plugin only
    works in Node.js 20 and 22, with no support for Node.js 18. It's
    possible to add support for Node.js 18.17.0+ however it complicates the
    plugin a lot - I don't think it's worth doing seeing as 18 goes
    end-of-life in April.
    
    The options for this task go against some of the Tool Kit design
    principals, mostly the fact that we rely on task options instead of a
    central config file. This is because `node --test` is configured
    completely via CLI or JS options and there's no config file.
    
    We also make a compromise to support both Node.js 20 _and_ 22. Node.js
    22 supports glob-patterns out of the box however Node.js 20 does not.
    This means we need to glob for files ourselves. I'm using the same
    patterns as Node.js 22's defaults so we can just remove these once we
    drop support for Node.js 20.
    
    Co-Authored-By: Ivo Murrell <ivomurrell@users.noreply.github.com>

---
+++ plugins/node-test/src/tasks/node-test.ts
@@ -0,0 +1,97 @@
+import { glob } from 'glob'
+import { pipeline } from 'node:stream/promises'
+import { run } from 'node:test'
+// Ignored because `node:test/reporters` is available in Node.js 20 but the root ESLint config
+// supports Node.js 18+. It seems overkill to add an overriding ESLint config
+// eslint-disable-next-line import/no-unresolved
+import { spec } from 'node:test/reporters'
+import { Task, TaskRunContext } from '@dotcom-tool-kit/base'
+import { ToolKitError } from '@dotcom-tool-kit/error'
+import { z } from 'zod'
+
+// We need to maintain this list of file patterns if we want to make this plugin work consistently
+// between Node.js 20 and 22. In future (when we drop Node.js 20 support) we will be able to remove
+// this and rely on the built-in patterns.
+// See https://nodejs.org/api/test.html#running-tests-from-the-command-line
+const defaultFilePatterns = [
+  '**/*.test.?(c|m)js',
+  '**/*-test.?(c|m)js',
+  '**/*_test.?(c|m)js',
+  '**/test-*.?(c|m)js',
+  '**/test.?(c|m)js',
+  '**/test/**/*.?(c|m)js'
+]
+
+// We don't want to run tests against files under "node_modules"
+const defaultIgnorePatterns = ['**/node_modules/**']
+
+const NodeTestSchema = z
+  .object({
+    concurrency: z
+      .number()
+      .int()
+      .positive()
+      .or(z.boolean())
+      .default(false)
+      .describe(
+        'The number of test processes to run in parallel. See https://nodejs.org/docs/latest-v20.x/api/test.html#runoptions'
+      ),
+    files: z
+      .array(z.string())
+      .default(defaultFilePatterns)
+      .describe('Glob patterns used to find test files to run'),
+    forceExit: z
+      .boolean()
+      .default(false)
+      .describe('Whether to force exit the test process if something is holding it open'),
+    ignore: z
+      .array(z.string())
+      .default(defaultIgnorePatterns)
+      .describe('Glob patterns for files to ignore when running tests'),
+    watch: z
+      .boolean()
+      .default(false)
+      .describe(
+        'Whether to run tests in "watch" mode, automatically rerunning them when related files change'
+      ),
+    customOptions: z
+      .record(z.unknown())
+      .optional()
+      .describe(
+        'Additional options to pass to the test runner, for those that Tool Kit does not explicitly support. See https://nodejs.org/docs/latest-v20.x/api/test.html#runoptions'
+      )
+  })
+  .describe('Run tests via the Node.js built-in test runner')
+
+export { NodeTestSchema as schema }
+
+export default class NodeTest extends Task<{ task: typeof NodeTestSchema }> {
+  async run({ cwd }: TaskRunContext) {
+    try {
+      const { concurrency, customOptions, files: filePatterns, forceExit, ignore, watch } = this.options
+      const files = await glob(filePatterns, { cwd, ignore })
+
+      let success = true
+      const testStream = run(Object.assign({ concurrency, files, forceExit, watch }, customOptions));
+      testStream.on('test:fail', () => {
+        success = false;
+      })
+      await pipeline(
+        testStream,
+        new spec(),
+        process.stdout
+      )
+      if (!success) {
+        throw new ToolKitError('some tests did not pass, error output has been logged above ☝️')
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        const error = new ToolKitError('node test failed to run')
+        error.details = err.message
+        throw error
+      } else {
+        throw err
+      }
+    }
+  }
+}
