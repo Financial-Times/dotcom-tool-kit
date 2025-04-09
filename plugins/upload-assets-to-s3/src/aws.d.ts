ecbac28ccdd2bcf076ad6f2c965de9542f87c6bb
Ivo Murrell <ivo.murrell@ft.com>
2025-04-09 15:13:03 +0100

    fix(upload-assets-to-s3): remove obsolete AWS SDK code

--- plugins/upload-assets-to-s3/src/aws.d.ts
+++
@@ -1,8 +0,0 @@
-// workaround for aws-sdk using DOM types in their definitions
-
-declare global {
-  /* eslint-disable @typescript-eslint/no-empty-interface */
-  export interface Blob {}
-}
-
-export {}
