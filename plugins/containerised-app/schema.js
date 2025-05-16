51020278da3ab64ea51dd261507c46e64209420c
Ivo Murrell <ivo.murrell@ft.com>
2025-05-16 12:35:55 +0100

    feat(containerised-app)!: allow hako environments to be configured
    This allows flexibility for when apps want to deploy to custom
    environments.
    
    We drop the old `multiregion` option as part of this change as it can
    now be better expressed by setting the `hakoProductionEnvironments`
    option, rather than carving out a special case for the ft-com
    environments. We could have preserved backwards-compatibility with this
    change – or even continued supporting the option – but it feels better
    to rip the bandage off now whilst not many apps have it set and to
    encourage one way of defining multiple environments.

--- plugins/containerised-app/schema.js
+++ plugins/containerised-app/schema.js
@@ -1,17 +1,43 @@
 const z = require('zod')
+const { HakoEnvironmentName } = require('@dotcom-tool-kit/hako/lib/tasks/deploy')
+const { styles } = require('@dotcom-tool-kit/logger')
 
-module.exports = z.object({
-  awsRoleArnStaging: z
-    .string()
-    .regex(/^arn:aws:iam::\d+:role\//, 'Role ARN must be a full IAM role ARN including account number')
-    .describe('the ARN of an IAM role to assume when deploying to staging'),
-  awsRoleArnProduction: z
-    .string()
-    .regex(/^arn:aws:iam::\d+:role\//, 'Role ARN must be a full IAM role ARN including account number')
-    .describe('the ARN of an IAM role to assume when deploying to production'),
-  // HACK: must be either `true` or `undefined` because of the way !toolkit/if-defined works
-  multiregion: z
-    .literal(true)
-    .optional()
-    .describe('Whether the app is deployed across both EU and US regions')
-})
+// We don't want to transform the environment yet as the value will get
+// substituted into the HakoDeploy task options where it will then get
+// transformed. The transform isn't idempotent so will result in a parse error
+// if applied twice.
+const HakoEnvironmentNameInner = HakoEnvironmentName.innerType()
+
+module.exports = z
+  .object({
+    awsRoleArnStaging: z
+      .string()
+      .regex(/^arn:aws:iam::\d+:role\//, 'Role ARN must be a full IAM role ARN including account number')
+      .describe('the ARN of an IAM role to assume when deploying to staging'),
+    awsRoleArnProduction: z
+      .string()
+      .regex(/^arn:aws:iam::\d+:role\//, 'Role ARN must be a full IAM role ARN including account number')
+      .describe('the ARN of an IAM role to assume when deploying to production'),
+    hakoReviewEnvironments: z
+      .array(HakoEnvironmentNameInner)
+      .default(['ft-com-test-eu'])
+      .describe('the set of Hako environments to deploy to in the deploy:review command'),
+    hakoStagingEnvironments: z
+      .array(HakoEnvironmentNameInner)
+      .default(['ft-com-test-eu'])
+      .describe('the set of Hako environments to deploy to in the deploy:staging command'),
+    hakoProductionEnvironments: z
+      .array(HakoEnvironmentNameInner)
+      .default(['ft-com-prod-eu'])
+      .describe('the set of Hako environments to deploy to in the deploy:production command')
+  })
+  .passthrough()
+  .refine((options) => !('multiregion' in options), {
+    message: `the option ${styles.code('multiregion')} has been replaced by ${styles.code(
+      'hakoReviewEnvironments'
+    )}, ${styles.code('hakoStagingEnvironments')}, and ${styles.code(
+      'hakoProductionEnvironments'
+    )}. set ${styles.code('hakoProductionEnvironments')} to ${styles.code(
+      '[ft-com-prod-eu, ft-com-prod-us]'
+    )} for equivalent behaviour.`
+  })
