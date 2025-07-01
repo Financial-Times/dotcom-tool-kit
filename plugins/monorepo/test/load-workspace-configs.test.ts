11cb518fd9a1cfa89c360f080cc243e46f5aee71
Kara Brightwell <kara@153.io>
2025-07-01 16:17:41 +0100

    test: use custom snapshot serialisers for all jest tests

--- plugins/monorepo/test/load-workspace-configs.test.ts
+++ plugins/monorepo/test/load-workspace-configs.test.ts
@@ -1,44 +1,9 @@
 import path from 'path'
 import LoadWorkspaceConfigs from '../src/load-workspace-configs'
 import winston, { Logger } from 'winston'
-import { stripAnsi } from '@relmify/jest-serializer-strip-ansi'
 import { removeRoots } from '../../../core/cli/test/helpers.ts'
 
 const logger = winston as unknown as Logger
-
-expect.addSnapshotSerializer(stripAnsi)
-
-expect.addSnapshotSerializer({
-  test(value) {
-    return value instanceof Error && value.name === 'AggregateError'
-  },
-
-  serialize(val, config, indentation, depth, refs, printer) {
-    return `AggregateError ${printer(val.message, config, indentation, depth, refs)} ${printer(
-      { errors: val.errors },
-      config,
-      indentation,
-      depth,
-      refs
-    )}`
-  }
-})
-
-expect.addSnapshotSerializer({
-  test(value) {
-    return value instanceof Error && value.constructor.name === 'ToolKitError'
-  },
-
-  serialize(val, config, indentation, depth, refs, printer) {
-    return `${printer(val.name, config, indentation, depth, refs)} ${printer(
-      val.message,
-      config,
-      indentation,
-      depth,
-      refs
-    )} ${printer({ details: val.details }, config, indentation, depth, refs)}`
-  }
-})
 
 describe('LoadWorkspaceConfigs', () => {
   beforeEach(() => {
