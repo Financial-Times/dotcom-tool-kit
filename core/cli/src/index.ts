97f2666abe11a35f85db025777e26be9b08a1394
Kara Brightwell <kara@153.io>
2025-07-03 15:06:39 +0100

    feat(cli): add a --printMergedOptions flag

--- core/cli/src/index.ts
+++ core/cli/src/index.ts
@@ -2,6 +2,7 @@ import { loadConfig } from './config'
 import type { Logger } from 'winston'
 import util from 'util'
 import { formatPluginTree } from './messages'
+import { loadHookInstallations } from './install'
 
 export { runTasks } from './tasks'
 export { shouldDisableNativeFetch } from './fetch'
@@ -19,4 +20,24 @@ export async function printConfig(logger: Logger): Promise<void> {
   const config = await loadConfig(logger, { validate: false, root: process.cwd() })
 
   logger.info(util.inspect(config, { depth: null, colors: true }))
+}
+
+export async function printMergedOptions(logger: Logger): Promise<void> {
+  const config = await loadConfig(logger, { validate: true, root: process.cwd() })
+  const hookInstallations = (await loadHookInstallations(logger, config)).unwrap('invalid hooks')
+
+  const mergedOptions = {
+    hooks: hookInstallations.map((h) => h.options),
+    plugins: Object.fromEntries(
+      Object.entries(config.pluginOptions).map(([pluginId, optionsForPlugin]) => [
+        pluginId,
+        optionsForPlugin.options
+      ])
+    ),
+    tasks: Object.fromEntries(
+      Object.entries(config.taskOptions).map(([taskId, optionsForTask]) => [taskId, optionsForTask.options])
+    )
+  }
+
+  logger.info(util.inspect(mergedOptions, { depth: null, colors: true }))
 }
