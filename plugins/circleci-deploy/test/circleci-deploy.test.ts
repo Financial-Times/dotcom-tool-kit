bf48208548797a1bb8650a5aba3ff809e72f9ea5
Ivo Murrell <ivo.murrell@ft.com>
2025-11-03 11:57:40 +0000

    feat(telemetry): add package to send client side metrics
    This package allows us to send metrics from Tool Kit invocations to a
    backend server, currently planned to be an instance of Vector running in
    AWS. We can use these metrics to get visibility for how people use Tool
    Kit, and where things may be going wrong. For now, I've only added one
    metric, which tracks what Tool Kit tasks are run and whether they
    completed successfully.
    
    The telemetry package uses a child process to allow us to send the
    metrics asynchronously and in the background once the main Tool Kit
    process exits, meaning there is minimal impact on the time it takes for
    Tool Kit commands to run on developer machines.
    
    Children of the metrics class can be created with arbitrary attributes
    overridden, and these classes are passed around between functions and
    stored as fields in Task and Hook classes, allowing important metadata,
    such as whether we're in CI or the name of the task currently running,
    to always be included in recorded events and scoped logically.

--- plugins/circleci-deploy/test/circleci-deploy.test.ts
+++ plugins/circleci-deploy/test/circleci-deploy.test.ts
@@ -1,6 +1,7 @@
 import * as YAML from 'yaml'
 import path from 'path'
 import CircleCi from '@dotcom-tool-kit/circleci/lib/circleci-config'
+import { MockTelemetryClient } from '@dotcom-tool-kit/telemetry'
 import winston, { Logger } from 'winston'
 import { loadConfig } from 'dotcom-tool-kit/lib/config'
 import { loadHookInstallations } from 'dotcom-tool-kit/lib/install'
@@ -11,7 +12,8 @@ describe('circleci-deploy', () => {
   describe('config integration test', () => {
     it('should generate a .circleci/config.yml with the base config from circleci-deploy/.toolkitrc.yml', async () => {
       const config = await loadConfig(logger, { root: path.resolve(__dirname, 'files', 'configs', 'base') })
-      const hookInstallationsPromise = loadHookInstallations(logger, config).then((validated) =>
+      const metrics = new MockTelemetryClient()
+      const hookInstallationsPromise = loadHookInstallations(logger, metrics, config).then((validated) =>
         validated.unwrap('hooks were invalid')
       )
 
