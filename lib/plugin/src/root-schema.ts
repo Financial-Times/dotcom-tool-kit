631a7c39e830db67820aad5894984c6423cd0470
Ivo Murrell <ivo.murrell@ft.com>
2025-11-24 10:07:26 +0000

    feat(plugin): add systemCode option to root Tool Kit options

--- lib/plugin/src/root-schema.ts
+++ lib/plugin/src/root-schema.ts
@@ -1,6 +1,11 @@
 import * as z from 'zod'
 
 export const RootSchema = z.object({
-  allowNativeFetch: z.boolean().default(false)
+  allowNativeFetch: z.boolean().default(false),
+  // TODO:IM:20251112 require this option in a future major version
+  systemCode: z
+    .string()
+    .optional()
+    .describe('Biz Ops system code or the package name prefixed with "npm:" otherwise')
 })
 export type RootOptions = z.infer<typeof RootSchema>
