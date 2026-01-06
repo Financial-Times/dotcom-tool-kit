f3cdf9469e347344cccfb91e0f7baae5d7181807
Kara Brightwell <kara@153.io>
2026-01-06 12:08:53 +0000

    backstage: remove the @dotcom-tool-kit/create
    this was designed for migrating an n-gage-based project to Tool Kit;
    it's outdated and not currently usable.

--- core/create/src/augmentations.d.ts
+++
@@ -1,8 +0,0 @@
-// workaround for simple-git using DOM types in their definitions
-
-declare global {
-  /* eslint-disable @typescript-eslint/no-empty-interface */
-  export interface AbortSignal {}
-}
-
-export {}
