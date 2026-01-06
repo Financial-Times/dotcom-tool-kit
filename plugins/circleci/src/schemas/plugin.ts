2bf942be381d8218dea35e3a2d00b492ef25765b
Kara Brightwell <kara@153.io>
2026-01-06 10:20:13 +0000

    feat(circleci)!: remove cypressImage option

--- plugins/circleci/src/schemas/plugin.ts
+++ plugins/circleci/src/schemas/plugin.ts
@@ -13,12 +13,6 @@ export default z
       .describe(
         'list of CircleCI [Node.js image versions](https://circleci.com/developer/images/image/cimg/node) to use. if more than one is provided, a [matrix build](https://circleci.com/docs/using-matrix-jobs/) will be generated in your CircleCI config.'
       ),
-    cypressImage: z
-      .string()
-      .optional()
-      .describe(
-        "the Cypress docker image to use. see https://github.com/cypress-io/cypress-docker-images for available images and tags. if this option is present, and you're using the [`circleci-deploy`](../circleci-deploy) plugin, this will override the default `node` executor for the `e2e-test-review` and `e2e-test-staging` jobs."
-      ),
     tagFilter: z
       .string()
       .or(z.literal(false))
